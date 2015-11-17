1490434a50daa81d2666c73de8b816e1b9d726b4
spuhpointer <madars.vitolins@gmail.com>
2015-11-17 10:57:19 +0200

    Fixes for uuid lib

--- tmsrv/xasrvutil.c
+++ tmsrv/xasrvutil.c
@@ -49,7 +49,7 @@
 
 #include "tmsrv.h"
 #include "../libatmisrv/srv_int.h"
-#include <uuid.h>
+#include <uuid/uuid.h>
 #include <xa_cmn.h>
 /*---------------------------Externs------------------------------------*/
 /*---------------------------Macros-------------------------------------*/
