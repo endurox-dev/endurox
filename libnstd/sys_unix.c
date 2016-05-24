533cffac3d9af6f4cc1217232ecefe03cd4b14d7
spuhpointer <madars.vitolins@d8corp.com>
2016-05-24 18:06:50 +0300

    More works on unix

--- libnstd/sys_unix.c
+++ libnstd/sys_unix.c
@@ -68,6 +68,20 @@
 #define HASH_ADD_MQD(head,ptrfield,add)                                          \
     HASH_ADD(hh,head,ptrfield,sizeof(mqd_t),add)
 
+
+#define EX_POLL_SETS_MAX            1024
+
+
+#define EX_EPOLL_API_ENTRY      {M_last_err = 0; M_last_err_msg[0] = EOS;}
+
+/* The index of the "read" end of the pipe */
+#define READ 0
+
+/* The index of the "write" end of the pipe */
+#define WRITE 1
+
+
+#define PIPE_POLL_IDX           0   /* where to pipe sits in poll */
 /*---------------------------Enums--------------------------------------*/
 /*---------------------------Typedefs-----------------------------------*/
 
@@ -124,7 +138,67 @@ public ex_epoll_set_t *M_psets = NULL; /* poll sets  */
  * - Once create is called, we prepare unnamed pipe2() for notify callback.
  */
 
+
+private int __thread M_last_err = 0;
+private char __thread M_last_err_msg[1024];  /* Last error message */
 /*---------------------------Prototypes---------------------------------*/
+
+
+/**
+ * We basically will use unix error codes
+ */
+private void ex_epoll_set_err(int error_code, const char *fmt, ...)
+{
+    char msg[MAX_TP_ERROR_LEN+1] = {EOS};
+    va_list ap;
+
+    va_start(ap, fmt);
+    (void) vsnprintf(msg, sizeof(msg), fmt, ap);
+    va_end(ap);
+
+    strcpy(M_last_err_msg, msg);
+    M_last_err = error_code;
+
+    NDRX_LOG(log_warn, "ex_epoll_set_err: %d (%s) (%s)",
+                    error_code, strerror(M_last_err), M_last_err_msg);
+    
+}
+
+/**
+ * Find FD in hash
+ */
+private ex_epoll_fds_t* fd_find(ex_epoll_set_t *pset, int fd)
+{
+    ex_epoll_fds_t*ret = NULL;
+    
+    HASH_FIND_INT( pset->fds, fd, ret);
+    
+    return ret;
+}
+
+/**
+ * FInd q descriptor in hash
+ */
+private ex_epoll_mqds_t* mqd_find(ex_epoll_set_t *pset, mqd_t mqd)
+{
+    ex_epoll_mqds_t*ret = NULL;
+    
+    HASH_FIND_MQD( pset->mqds, mqd, ret);
+    
+    return ret;
+}
+
+/**
+ * Find polling set
+ */
+private ex_epoll_set_t* pset_find(int fd)
+{
+    ex_epoll_set_t *ret = NULL;
+    
+    HASH_FIND_INT( M_psets, fd, ret);
+    
+    return ret;
+}
 
 /**
  * Wrapper for epoll_ctl, for standard file descriptors
@@ -136,6 +210,10 @@ public ex_epoll_set_t *M_psets = NULL; /* poll sets  */
  */
 public int ex_epoll_ctl(int epfd, int op, int fd, struct ex_epoll_event *event)
 {
+    EX_EPOLL_API_ENTRY;
+    
+    ex_epoll_set_err(ENOSYS, "Not yet supported.");
+    
     return FAIL;
 }
 
@@ -149,6 +227,10 @@ public int ex_epoll_ctl(int epfd, int op, int fd, struct ex_epoll_event *event)
  */
 public int ex_epoll_ctl_mq(int epfd, int op, mqd_t fd, struct ex_epoll_event *event)
 {
+    EX_EPOLL_API_ENTRY;
+    
+    ex_epoll_set_err(ENOSYS, "Not yet supported.");
+    
     return FAIL;
 }
 
@@ -159,7 +241,75 @@ public int ex_epoll_ctl_mq(int epfd, int op, mqd_t fd, struct ex_epoll_event *ev
  */
 public int ex_epoll_create(int size)
 {
-    return FAIL;
+    int ret = SUCCEED;
+    int i = 1;
+    ex_epoll_set_t *set;
+    
+    EX_EPOLL_API_ENTRY;
+    
+    while (NULL!=(set=pset_find(i)) && i < EX_POLL_SETS_MAX)
+    {
+        i++;
+    }
+    
+    /* we must have free set */
+    if (NULL!=set)
+    {
+        ex_epoll_set_err(EMFILE, "Max ex_epoll_sets_reached");
+        set = NULL;
+        FAIL_OUT(ret);
+    }
+    
+    NDRX_LOG(log_info, "Creating ex_epoll set: %d", i);
+    
+    if (SUCCEED!=(set = (ex_epoll_set_t *)calloc(1, sizeof(*set))))
+    {
+        ex_epoll_set_err(errno, "Failed to alloc: %d bytes", sizeof(*set));
+        FAIL_OUT(ret);
+    }
+    
+    if (pipe2(set->wakeup_pipe, O_NONBLOCK) == -1)
+    {
+        ex_epoll_set_err(errno, "pipe2 failed");
+        FAIL_OUT(ret);
+    }
+    
+    if (NULL==(set->pollfd = calloc(1, sizeof(struct pollfd))))
+    {
+        ex_epoll_set_err(errno, "calloc for pollfd failed");
+        FAIL_OUT(ret);
+    }
+    
+    /* So wait for events here in the pip form Q thread */
+    set->fdtab[PIPE_POLL_IDX].fd = set->wakeup_pipe[READ];
+    set->fdtab[PIPE_POLL_IDX].events = POLLIN;
+    
+    
+    NDRX_LOG(log_info, "ex_epoll_create succeed, fd=%d", i);
+out:
+
+    if (SUCCEED!=ret)
+    {
+        if (NULL!=set && set->wakeup_pipe[READ])
+        {
+            close(set->wakeup_pipe[READ]);
+        }
+        
+        if (NULL!=set && set->wakeup_pipe[WRITE])
+        {
+            close(set->wakeup_pipe[WRITE]);
+        }
+            
+        if (NULL!=set)
+        {
+            free((char *)set);
+        }
+        
+        return FAIL;
+        
+    }
+
+    return i;
 }
 
 /**
@@ -167,6 +317,9 @@ public int ex_epoll_create(int size)
  */
 public int ex_epoll_close(int fd)
 {
+    
+    ex_epoll_set_err(ENOSYS, "Not yet supported.");
+    
     return FAIL;
 }
 
@@ -183,6 +336,10 @@ public int ex_epoll_close(int fd)
  */
 public int ex_epoll_wait(int epfd, struct ex_epoll_event *events, int maxevents, int timeout)
 {
+    EX_EPOLL_API_ENTRY;
+    
+    ex_epoll_set_err(ENOSYS, "Not yet supported.");
+    
     return FAIL;
 }
 
@@ -192,7 +349,7 @@ public int ex_epoll_wait(int epfd, struct ex_epoll_event *events, int maxevents,
  */
 public int ex_epoll_errno(void)
 {
-    return FAIL;
+    return M_last_err;
 }
 
 /**
@@ -202,7 +359,9 @@ public int ex_epoll_errno(void)
  */
 public char * ex_poll_strerror(int err)
 {
-    static __thread char ret[] = "Not implemented.";
+    static __thread char ret[1024];
+    
+    sprintf(ret, "%s (last error: %s)", strerror(err), M_last_err;
     
     return ret;
 }
