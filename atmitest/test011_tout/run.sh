cd47a414e9f95896f06cfc2f71f92cdece57150b
Madars Vitolins <madars.vitolins@gmail.com>
2016-05-29 16:52:00 +0000

    Fixes on killall command

--- atmitest/test011_tout/run.sh
+++ atmitest/test011_tout/run.sh
@@ -73,7 +73,7 @@ function go_out {
     xadmin down -y
 
     # If some alive stuff left...
-    killall -9 atmiclt_$TESTNO
+    xadmin killall -9 atmiclt_$TESTNO
 
     popd 2>/dev/null
     exit $1
