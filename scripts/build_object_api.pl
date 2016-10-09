f80b331969c96932d9187472438ebe30f9ccbd5b
spuhpointer <madars.vitolins@gmail.com>
2016-10-09 16:01:34 +0300

    Works on O-API

--- scripts/build_object_api.pl
+++ scripts/build_object_api.pl
@@ -19,11 +19,14 @@ if (!$opt_ret || $opt_h || !$opt_i || !$opt_o)
         exit 1;
 }
 
+my $M_name_upper = uc $opt_o;
+$M_name_upper = "O$M_name_upper";
+$M_name = "o$opt_o";
 
 #
 # Remove whitespace from string
 #
-sub remove_white_space  {
+sub remove_white_space {
 
 	my $ret = shift;
 	
@@ -38,52 +41,261 @@ sub remove_white_space  {
 # Object API output file handling
 ################################################################################
 
+
+
+#
+# Have some file handers, one for header
+# and one for c
+#
+
+
+################################################################################
+# Open C header
+################################################################################
 sub open_h {
 
-	$message = "UNK!";
+	$M_h_name = "${M_name}.h";
+	print "Opening [$M_h_name]\n";
+	open($M_h_fd, '>', $M_h_name) or die "Could not open file '$M_h_name' $!";
 
-	if ($opt_o =~ /xatmi/)
+	my $title = "";
+
+	if ($M_name=~/oxatmi/)
 	{
+		$title = "XATMI Object API header (auto-generated)";
+	}
+	elsif($M_name=~/oubf/)
+	{
+		$title = "UBF Object API header (auto-generated)";
+	}
 	
-$message = <<'END_MESSAGE';
+my $message = <<"END_MESSAGE";
+/* 
+** $title
+**
+** \@file ${M_name}.h
+** 
+** -----------------------------------------------------------------------------
+** Enduro/X Middleware Platform for Distributed Transaction Processing
+** Copyright (C) 2015, ATR Baltic, SIA. All Rights Reserved.
+** This software is released under one of the following licenses:
+** GPL or ATR Baltic's license for commercial use.
+** -----------------------------------------------------------------------------
+** GPL license:
+** 
+** This program is free software; you can redistribute it and/or modify it under
+** the terms of the GNU General Public License as published by the Free Software
+** Foundation; either version 2 of the License, or (at your option) any later
+** version.
+**
+** This program is distributed in the hope that it will be useful, but WITHOUT ANY
+** WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+** PARTICULAR PURPOSE. See the GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License along with
+** this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+** Place, Suite 330, Boston, MA 02111-1307 USA
+**
+** -----------------------------------------------------------------------------
+** A commercial use license is available from ATR Baltic, SIA
+** contact\@atrbaltic.com
+** -----------------------------------------------------------------------------
+*/
+#ifndef __${M_name_upper}_H
+#define __${M_name_upper}_H
+
+#ifdef  __cplusplus
+extern "C" {
+#endif
+/*---------------------------Includes-----------------------------------*/
+#include <stdint.h>
+#include <ubf.h>
+#include <atmi.h>
+/*---------------------------Externs------------------------------------*/
+/*---------------------------Macros-------------------------------------*/
+/*---------------------------Enums--------------------------------------*/
+/*---------------------------Typedefs-----------------------------------*/
+/*---------------------------Globals------------------------------------*/
+/*---------------------------Statics------------------------------------*/
+/*---------------------------Prototypes---------------------------------*/
+END_MESSAGE
+	
+print $M_h_fd $message;
+
+}
+
+################################################################################
+# Open C source file
+################################################################################
+sub open_c {
+
+
+	$M_c_name = "${M_name}.c";
+	print "Opening [$M_c_name]\n";
+	open($M_c_fd, '>', $M_c_name) or die "Could not open file '$M_c_name' $!";
+
+	my $title = "";
+
+	if ($M_name=~/oxatmi/)
+	{
+		$title = "XATMI Object API code (auto-generated)";
+	}
+	elsif($M_name=~/oubf/)
+	{
+		$title = "UBF Object API code (auto-generated)";
+	}
+my $message = <<"END_MESSAGE";
+/* 
+** $title
+**
+** @file ${M_name}.c
+** 
+** -----------------------------------------------------------------------------
+** Enduro/X Middleware Platform for Distributed Transaction Processing
+** Copyright (C) 2015, ATR Baltic, SIA. All Rights Reserved.
+** This software is released under one of the following licenses:
+** GPL or ATR Baltic's license for commercial use.
+** -----------------------------------------------------------------------------
+** GPL license:
+** 
+** This program is free software; you can redistribute it and/or modify it under
+** the terms of the GNU General Public License as published by the Free Software
+** Foundation; either version 2 of the License, or (at your option) any later
+** version.
+**
+** This program is distributed in the hope that it will be useful, but WITHOUT ANY
+** WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+** PARTICULAR PURPOSE. See the GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License along with
+** this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+** Place, Suite 330, Boston, MA 02111-1307 USA
+**
+** -----------------------------------------------------------------------------
+** A commercial use license is available from ATR Baltic, SIA
+** contact\@atrbaltic.com
+** -----------------------------------------------------------------------------
+*/
+#include <string.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <memory.h>
+#include <errno.h>
+#include <dlfcn.h>
+
+#include <atmi.h>
+#include <atmi_shm.h>
+#include <ndrstandard.h>
+#include <ndebug.h>
+#include <ndrxd.h>
+#include <ndrxdcmn.h>
+#include <userlog.h>
+#include <xa_cmn.h>
+/*---------------------------Externs------------------------------------*/
+/*---------------------------Macros-------------------------------------*/
+/*---------------------------Enums--------------------------------------*/
+/*---------------------------Typedefs-----------------------------------*/
+/*---------------------------Globals------------------------------------*/
+/*---------------------------Statics------------------------------------*/
+/*---------------------------Prototypes---------------------------------*/
+END_MESSAGE
+
+print $M_c_fd $message;
+
+}
+
+
+################################################################################
+# Close C header
+################################################################################
+sub close_h {
+$message = <<"END_MESSAGE";
+#endif  /* __${M_name_upper}_H */
 
 END_MESSAGE
 
-	}
-	elsif ($opt_o =~/ubf/)
-	{
-	$message = <<'END_MESSAGE';
-		Dear $name,
-		
-		this is a message I plan to send to you.
-		
-		regards
-		the Perl Maven
-		END_MESSAGE
-	}
-	
+print $M_h_fd $message;
+
+close($M_h_fd)
+
 }
 
-sub open_c {
-	
-}
-
-sub write_h {
-	
-}
-
-sub write_c {
-	
-}
-
-sub close_h {
-	
-}
-
+################################################################################
+# Close C source file
+################################################################################
 sub close_c {
+
+$message = <<"END_MESSAGE";
+
+END_MESSAGE
+
+print $M_c_fd $message;
+
+close($M_c_fd)
+
 	
 }
 
+
+################################################################################
+# Generate Object API func signature
+################################################################################
+sub gen_sig {
+
+	my $func_type = shift;
+	my $func_name = shift;
+	my $func_args_list = shift;
+	
+	return "$func_type O$func_name(TPCONTEXT_T *context, $func_args_list)";
+}
+
+################################################################################
+# Write C object API header func
+################################################################################
+sub write_h {
+	my $sig = shift;
+	print $M_h_fd "export NDRX_API $sig;\n";
+}
+
+################################################################################
+# Write C object API function
+################################################################################
+sub write_c {
+	my $func_type = $_[1];
+	my $func_name = $_[2];
+	my $sig = $_[3];
+	
+	my @func_arg_type = @{$_[4]};
+	my @func_arg_name = @{$_[5]};
+	my @func_arg_def = @{$_[6]};
+	
+	#foreach my $type( @func_arg_type )
+	#{
+		#print $M_c_fd "$func_name - $sig - $type\n";
+	#}
+	
+	
+	if ($func_type~=m/^int$/)
+	{
+		# Generate int func
+	} 
+	elsif ($func_type~=m/^void$/)
+	{
+# 		# Generate void func
+	}
+	else
+	{
+		# return if unsupported
+		print "!!! Unsupported return type [$func_type]\n";
+		return;
+	}
+	
+}
+
+
+open_h();
+open_c();
 ################################################################################
 # Open source file
 ################################################################################
@@ -123,21 +335,27 @@ NEXT: while( my $line = <$info>)
 	print "Processing line [$line]\n";
 	
 	#
-	# Skip the specific symbols
+	# Skip the specific symbols, per output module
 	#
-	
-	if ($line =~ m/G_tpsvrinit__/
-		|| $line =~ m/G_tpsvrdone__/ 
-		|| $line =~ m/ndrx_main/ 
-		|| $line =~ m/ndrx_main_integra/ 
-		|| $line =~ m/ndrx_atmi_tls_get/
-		|| $line =~ m/ndrx_atmi_tls_set/
-		|| $line =~ m/ndrx_atmi_tls_free/
-		|| $line =~ m/ndrx_atmi_tls_new/
-		)
+	if ($M_name =~ m/$oatmi^/)
 	{
-		print "skip - next\n";
-		next NEXT;
+		if ($line =~ m/G_tpsvrinit__/
+			|| $line =~ m/G_tpsvrdone__/ 
+			|| $line =~ m/ndrx_main/ 
+			|| $line =~ m/ndrx_main_integra/ 
+			|| $line =~ m/ndrx_atmi_tls_get/
+			|| $line =~ m/ndrx_atmi_tls_set/
+			|| $line =~ m/ndrx_atmi_tls_free/
+			|| $line =~ m/ndrx_atmi_tls_new/
+			)
+		{
+			print "skip - next\n";
+			next NEXT;
+		}
+	}
+	elsif ($M_name =~ m/$osrvxatmi^/)
+	{
+		# Server mode XATMI Object API
 	}
 	
 	# Process next, parse arguments and their types
@@ -187,14 +405,25 @@ NEXT: while( my $line = <$info>)
 			
 			print "got param (pair: [$pair]), type: [$type], name: [$name], def: [$def]\n";
 			push @func_arg_type, $type;
-			push @func_arg_name, $name;
-				
+			push @func_arg_name, $name;	
 		}
+		
+		my $sig = gen_sig($func_type, $func_name, $func_args_list);
+		
+		# Write header;
+		
+		write_h($sig);
+		
+		write_c($func_type, $func_name, $sig, \@func_arg_type, \@func_arg_name, \@func_arg_def);
+	
+		
 	}
     }
 }
 
 close $info;
 
+close_h();
+close_c();
 
 
