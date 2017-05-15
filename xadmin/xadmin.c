e15ba69b3e48bd7535fa389e404d0f9b02636e1e
spuhpointer <madars.vitolins@d8corp.com>
2017-05-15 12:58:29 +0300

    Updated copyright statement Latvian sia -> International Ltd.

--- xadmin/xadmin.c
+++ xadmin/xadmin.c
@@ -795,8 +795,8 @@ int main(int argc, char** argv) {
         fprintf(stderr, "%s, build %s %s, using %s for %s (%ld bits)\n\n", NDRX_VERSION, 
                     __DATE__, __TIME__, ndrx_epoll_mode(), NDRX_BUILD_OS_NAME, sizeof(void *)*8);
         fprintf(stderr, "Enduro/X Middleware Platform for Distributed Transaction Processing\n");
-        fprintf(stderr, "Copyright (C) 2015, 2016 ATR Baltic, SIA.\n");
-        fprintf(stderr, "Copyright (C) 2017 Mavimax, SIA. All Rights Reserved.\n\n");
+        fprintf(stderr, "Copyright (C) 2015, 2016 ATR Baltic Ltd.\n");
+        fprintf(stderr, "Copyright (C) 2017 Mavimax Ltd. All Rights Reserved.\n\n");
         fprintf(stderr, "This software is released under one of the following licenses:\n");
         fprintf(stderr, "GPLv2 (or later) or ATR Baltic's license for commercial use.\n\n");
     }
