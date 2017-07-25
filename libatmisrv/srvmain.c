ca7976c3f9613628b019ef1ff314d2d01140bf27
spuhpointer <madars.vitolins@gmail.com>
2017-07-25 11:16:05 +0300

    Added missing headers

--- libatmisrv/srvmain.c
+++ libatmisrv/srvmain.c
@@ -34,6 +34,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
+#include <fcntl.h>
+
 #include <ndrstandard.h>
 #include <ndebug.h>
 #include <utlist.h>
--- libnstd/ndebug.c
+++ libnstd/ndebug.c
@@ -36,6 +36,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <fcntl.h>
 
 #include <unistd.h>
 #include <stdarg.h>
