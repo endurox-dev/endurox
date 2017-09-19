b24f67bb1abbb8bd13c6976fdedf2c3b9c5bf896
Test user <user1@freebsd11.silodev.eu>
2017-09-19 22:05:40 +0300

    Fixes for freebsd

--- libubf/view_null.c
+++ libubf/view_null.c
@@ -436,7 +436,7 @@ expublic int ndrx_Bvselinit_int(ndrx_typedview_t *v, ndrx_typedview_field_t *f,
                         len = dim_size-1; /* Place for eos */
                     }
                     
-                    NDRX_STRNCPY(fld_offs, f->nullval_bin, len)
+                    NDRX_STRNCPY(fld_offs, f->nullval_bin, len);
                     
                     fld_offs[len] = EXEOS;                    
                 }
