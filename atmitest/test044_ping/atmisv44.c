641346e411444caf35df7b731dbb8c01ff223a68
Test user <user1@freebsd11.silodev.eu>
2017-12-21 11:37:28 +0200

    Fixes on unit test

--- atmitest/test044_ping/atmisv44.c
+++ atmitest/test044_ping/atmisv44.c
@@ -63,22 +63,7 @@ void TESTSV (TPSVCINFO *p_svc)
     /* Just print the buffer */
     Bprint(p_ub);
     
-    if (EXFAIL==Bget(p_ub, T_STRING_FLD, 0, testbuf, 0))
-    {
-        NDRX_LOG(log_error, "TESTERROR: Failed to get T_STRING_FLD: %s", 
-                 Bstrerror(Berror));
-        ret=EXFAIL;
-        goto out;
-    }
-    
-    if (0!=strcmp(testbuf, VALUE_EXPECTED))
-    {
-        NDRX_LOG(log_error, "TESTERROR: Expected: [%s] got [%s]",
-            VALUE_EXPECTED, testbuf);
-        ret=EXFAIL;
-        goto out;
-    }
-        
+    sleep(99999); /* will be killed */
     
 out:
     tpreturn(  ret==EXSUCCEED?TPSUCCESS:TPFAIL,
