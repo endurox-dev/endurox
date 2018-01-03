35bbbddc7cb3d5c063adb7b659450f50fdd4a2b8
Test user <user1@freebsd11.silodev.eu>
2018-01-03 15:37:43 +0200

    Fixed build warnings on freebsd

--- ubftest/test_nstd_b64.c
+++ ubftest/test_nstd_b64.c
@@ -69,13 +69,13 @@ Ensure(test_nstd_base64)
     char str[TPCONVMAXSTR];
     size_t outlen;
     
-    assert_not_equal(ndrx_xa_base64_encode(M_bin_data, sizeof(M_bin_data), 
+    assert_not_equal(ndrx_xa_base64_encode((unsigned char *)M_bin_data, sizeof(M_bin_data), 
             &outlen, str), NULL);
     
     str[outlen] = EXEOS;
     NDRX_LOG(log_debug, "Got b64 string: [%s]", str);
     
-    assert_not_equal(ndrx_xa_base64_decode(str, strlen(str),
+    assert_not_equal(ndrx_xa_base64_decode((unsigned char *)str, strlen(str),
             &outlen, bin), NULL);
     
     assert_equal(memcmp(bin, M_bin_data, sizeof(M_bin_data)), 0);
