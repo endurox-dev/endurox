47ff1bdb68f9d0e4da3c729230f83382646e8a9e
Madars Vitolins <madars.vitolins@gmail.com>
2018-02-19 02:17:14 +0200

    Works on cache testing

--- atmitest/test048_cache/run.sh
+++ atmitest/test048_cache/run.sh
@@ -49,6 +49,7 @@ run_test "01_run"
 run_test "02_run"
 run_test "03_run"
 run_test "04_run"
+run_test "05_run_refresh"
 
 echo "*** SUMMARY $M_tests tests executed. $M_ok passes, $M_fail failures"
 
