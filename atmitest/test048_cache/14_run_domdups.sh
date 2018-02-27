a6b5744ca41eb9ef868c1948ad75df65af80f26e
Madars Vitolins <madars.vitolins@gmail.com>
2018-02-28 01:59:01 +0200

    Works on cache tests

--- atmitest/test048_cache/14_run_domdups.sh
+++ atmitest/test048_cache/14_run_domdups.sh
@@ -144,37 +144,45 @@ echo "Domain 1 info"
 set_dom1;
 xadmin psc
 xadmin ppm
+xadmin sc -t CACHED
 xadmin pc
 
 echo "Domain 2 info"
 set_dom2;
 xadmin psc
 xadmin ppm
+xadmin sc -t CACHED
 xadmin pc
 
 echo "Domain 3 info"
 set_dom3;
 xadmin psc
 xadmin ppm
+xadmin sc -t CACHED
 xadmin pc
 
 
 echo "Running off client on domain 1"
-
 set_dom1;
 
 (time ./testtool48 -sTESTSV14 -b '{"T_STRING_FLD":"KEY1"}' \
     -m '{"T_STRING_FLD":"KEY1"}' \
-    -cY -n100 -fY 2>&1) > ./13_testtool48.log
+    -cY -n1 -fY -l 2>&1) > ./13_testtool48.log
 
 if [ $? -ne 0 ]; then
     echo "testtool48 failed (1)"
     go_out 1
 fi
 
-(time ./testtool48 -sTESTSV14 -b '{"T_STRING_FLD":"KEY2"}' \
-    -m '{"T_STRING_FLD":"KEY2"}' \
-    -cY -n91 -fY 2>&1) >> ./13_testtool48.log
+sleep 1
+
+echo "Running off client on domain 2"
+set_dom2;
+
+
+(time ./testtool48 -sTESTSV14 -b '{"T_STRING_FLD":"KEY1"}' \
+    -m '{"T_STRING_FLD":"KEY1"}' \
+    -cY -n1 -fY -l 2>&1) >> ./13_testtool48.log
 
 if [ $? -ne 0 ]; then
     echo "testtool48 failed (2)"
@@ -182,200 +190,12 @@ if [ $? -ne 0 ]; then
 fi
 
 
-(time ./testtool48 -sTESTSV14 -b '{"T_STRING_FLD":"KEY3"}' \
-    -m '{"T_STRING_FLD":"KEY3"}' \
-    -cY -n92 -fY 2>&1) >> ./13_testtool48.log
-
-if [ $? -ne 0 ]; then
-    echo "testtool48 failed (3)"
-    go_out 1
-fi
-
-
-(time ./testtool48 -sTESTSV14 -b '{"T_STRING_FLD":"KEY4"}' \
-    -m '{"T_STRING_FLD":"KEY4"}' \
-    -cY -n93 -fY 2>&1) >> ./13_testtool48.log
-
-if [ $? -ne 0 ]; then
-    echo "testtool48 failed (4)"
-    go_out 1
-fi
-
-(time ./testtool48 -sTESTSV14 -b '{"T_STRING_FLD":"KEY5"}' \
-    -m '{"T_STRING_FLD":"KEY5"}' \
-    -cY -n94 -fY 2>&1) >> ./13_testtool48.log
-
-if [ $? -ne 0 ]; then
-    echo "testtool48 failed (5)"
-    go_out 1
-fi
-
-
-(time ./testtool48 -sTESTSV14 -b '{"T_STRING_FLD":"KEY6"}' \
-    -m '{"T_STRING_FLD":"KEY6"}' \
-    -cY -n95 -fY 2>&1) >> ./13_testtool48.log
-
-if [ $? -ne 0 ]; then
-    echo "testtool48 failed (6)"
-    go_out 1
-fi
-
-
-(time ./testtool48 -sTESTSV14 -b '{"T_STRING_FLD":"KEY7"}' \
-    -m '{"T_STRING_FLD":"KEY7"}' \
-    -cY -n96 -fY 2>&1) >> ./13_testtool48.log
-
-if [ $? -ne 0 ]; then
-    echo "testtool48 failed (7)"
-    go_out 1
-fi
-
-(time ./testtool48 -sTESTSV14 -b '{"T_STRING_FLD":"KEY8"}' \
-    -m '{"T_STRING_FLD":"KEY8"}' \
-    -cY -n97 -fY 2>&1) >> ./13_testtool48.log
-
-if [ $? -ne 0 ]; then
-    echo "testtool48 failed (8)"
-    go_out 1
-fi
-
-
-(time ./testtool48 -sTESTSV14 -b '{"T_STRING_FLD":"KEY9"}' \
-    -m '{"T_STRING_FLD":"KEY9"}' \
-    -cY -n98 -fY 2>&1) >> ./13_testtool48.log
-
-if [ $? -ne 0 ]; then
-    echo "testtool48 failed (9)"
-    go_out 1
-fi
-
-
-(time ./testtool48 -sTESTSV14 -b '{"T_STRING_FLD":"KEY10"}' \
-    -m '{"T_STRING_FLD":"KEY10"}' \
-    -cY -n99 -fY 2>&1) >> ./13_testtool48.log
-
-if [ $? -ne 0 ]; then
-    echo "testtool48 failed (10)"
-    go_out 1
-fi
-
-
-(time ./testtool48 -sTESTSV14 -b '{"T_STRING_FLD":"KEY11"}' \
-    -m '{"T_STRING_FLD":"KEY11"}' \
-    -cY -n100 -fY 2>&1) >> ./13_testtool48.log
-
-if [ $? -ne 0 ]; then
-    echo "testtool48 failed (11)"
-    go_out 1
-fi
-
-echo "Checking keys in domain 1..."
+echo "Printing dom 1 keys"
 set_dom1;
+xadmin pc 
 
-ensure_field db13 SV14KEY11 T_STRING_FLD KEY11 1
-ensure_field db13 SV14KEY10 T_STRING_FLD KEY10 1
-ensure_field db13 SV14KEY9 T_STRING_FLD KEY9 1
-ensure_field db13 SV14KEY8 T_STRING_FLD KEY8 1
-ensure_field db13 SV14KEY7 T_STRING_FLD KEY7 1
-ensure_field db13 SV14KEY6 T_STRING_FLD KEY6 1
-ensure_field db13 SV14KEY5 T_STRING_FLD KEY5 1
-ensure_field db13 SV14KEY4 T_STRING_FLD KEY4 1
-ensure_field db13 SV14KEY3 T_STRING_FLD KEY3 1
-ensure_field db13 SV14KEY2 T_STRING_FLD KEY2 1
-ensure_field db13 SV14KEY1 T_STRING_FLD KEY1 1
-
-#
-# Let events to process if any queues..
-#
-sleep 5
-
-echo "Checking keys in domain 2..."
+echo "Printing dom 2 keys"
 set_dom2;
-
-ensure_field db13 SV14KEY11 T_STRING_FLD KEY11 1
-ensure_field db13 SV14KEY10 T_STRING_FLD KEY10 1
-ensure_field db13 SV14KEY9 T_STRING_FLD KEY9 1
-ensure_field db13 SV14KEY8 T_STRING_FLD KEY8 1
-ensure_field db13 SV14KEY7 T_STRING_FLD KEY7 1
-ensure_field db13 SV14KEY6 T_STRING_FLD KEY6 1
-ensure_field db13 SV14KEY5 T_STRING_FLD KEY5 1
-ensure_field db13 SV14KEY4 T_STRING_FLD KEY4 1
-ensure_field db13 SV14KEY3 T_STRING_FLD KEY3 1
-ensure_field db13 SV14KEY2 T_STRING_FLD KEY2 1
-ensure_field db13 SV14KEY1 T_STRING_FLD KEY1 1
-
-echo "Checking keys in domain 3..."
-set_dom3;
-
-ensure_field db13 SV14KEY11 T_STRING_FLD KEY11 1
-ensure_field db13 SV14KEY10 T_STRING_FLD KEY10 1
-ensure_field db13 SV14KEY9 T_STRING_FLD KEY9 1
-ensure_field db13 SV14KEY8 T_STRING_FLD KEY8 1
-ensure_field db13 SV14KEY7 T_STRING_FLD KEY7 1
-ensure_field db13 SV14KEY6 T_STRING_FLD KEY6 1
-ensure_field db13 SV14KEY5 T_STRING_FLD KEY5 1
-ensure_field db13 SV14KEY4 T_STRING_FLD KEY4 1
-ensure_field db13 SV14KEY3 T_STRING_FLD KEY3 1
-ensure_field db13 SV14KEY2 T_STRING_FLD KEY2 1
-ensure_field db13 SV14KEY1 T_STRING_FLD KEY1 1
-
-
-echo "*************************************************************************"
-echo "Testing delete broadcast... (single record)"
-echo "*************************************************************************"
-
-echo "Domain 1"
-set_dom1;
-
-xadmin ci -d db13 -k SV14KEY6
-
-ensure_field db13 SV14KEY7 T_STRING_FLD KEY7 1
-ensure_field db13 SV14KEY6 T_STRING_FLD KEY6 0
-ensure_field db13 SV14KEY5 T_STRING_FLD KEY5 1
-
-echo "Domain 2"
-set_dom2;
-
-ensure_field db13 SV14KEY7 T_STRING_FLD KEY7 1
-ensure_field db13 SV14KEY6 T_STRING_FLD KEY6 0
-ensure_field db13 SV14KEY5 T_STRING_FLD KEY5 1
-
-echo "Domain 3"
-set_dom3;
-
-ensure_field db13 SV14KEY7 T_STRING_FLD KEY7 1
-ensure_field db13 SV14KEY6 T_STRING_FLD KEY6 0
-ensure_field db13 SV14KEY5 T_STRING_FLD KEY5 1
-
-
-echo "*************************************************************************"
-echo "Testing delete broadcast... (regexp)"
-echo "*************************************************************************"
-
-echo "Domain 1"
-set_dom1;
-
-xadmin ci -d db13 -k SV14KEY1. -r
-
-ensure_field db13 SV14KEY11 T_STRING_FLD KEY11 0
-ensure_field db13 SV14KEY10 T_STRING_FLD KEY10 0
-ensure_field db13 SV14KEY9 T_STRING_FLD KEY9 1
-
-echo "Domain 2"
-set_dom2;
-
-ensure_field db13 SV14KEY11 T_STRING_FLD KEY11 0
-ensure_field db13 SV14KEY10 T_STRING_FLD KEY10 0
-ensure_field db13 SV14KEY9 T_STRING_FLD KEY9 1
-
-echo "Domain 3"
-set_dom3;
-
-ensure_field db13 SV14KEY11 T_STRING_FLD KEY11 0
-ensure_field db13 SV14KEY10 T_STRING_FLD KEY10 0
-ensure_field db13 SV14KEY9 T_STRING_FLD KEY9 1
-
-
 
 go_out $RET
 
