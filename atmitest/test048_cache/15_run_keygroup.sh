e8fb366c0ba4c3b2d38c2924f633201344760d4b
Madars Vitolins <madars.vitolins@gmail.com>
2018-03-07 17:41:52 +0200

    Works on cache

--- atmitest/test048_cache/15_run_keygroup.sh
+++ atmitest/test048_cache/15_run_keygroup.sh
@@ -354,7 +354,7 @@ T_SHORT_2_FLD	1
 EOF
 
 echo "Sleeping 3 to broadcast delete... (1)"
-sleep 3
+sleep 1
 
 echo "Testing DOM 1"
 
@@ -445,7 +445,7 @@ T_SHORT_2_FLD	1
 EOF
 
 echo "Sleeping 3 to broadcast delete... (2)"
-sleep 3
+sleep 1
 
 echo "Testing DOM 1"
 ensure_field g@db15 SV15KEY1 EX_CACHE_OPEXPR SV15KEY1-SV158 1
