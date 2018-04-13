41ed86a5c057239460d4bed1d0237096421411fe
Madars Vitolins <madars.vitolins@gmail.com>
2018-04-13 12:04:58 +0300

    Added crc32 instead of hostname

--- include/nstdutil.h
+++ include/nstdutil.h
@@ -133,6 +133,11 @@ extern NDRX_API void * ndrx_nstd_tls_new(int auto_destroy, int auto_set);
 /* Platform: */
 extern NDRX_API long ndrx_platf_stack_get_size(void);
 
+
+extern NDRX_API unsigned long ndrx_Crc32_ComputeBuf( unsigned long inCrc32, const void *buf,
+                                       size_t bufLen );
+
+
 #ifdef	__cplusplus
 }
 #endif
