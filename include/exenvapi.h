82b72adf932a8f3784a33fc63ff2fc6bcd567bed
Madars Vitolins <madars.vitolins@gmail.com>
2018-06-18 17:46:20 +0300

    Fixes on header

--- include/exenvapi.h
+++ include/exenvapi.h
@@ -57,7 +57,7 @@ extern int ndrx_ndrxconf_envs_parse(xmlDocPtr doc, xmlNodePtr cur,
 
 extern void ndrx_ndrxconf_envs_groups_free(ndrx_env_group_t **grouphash);
 extern void ndrx_ndrxconf_envs_grouplists_free(ndrx_env_grouplist_t **grouplist);
-extern void  ndrx_ndrxconf_envs_envs_free(ndrx_env_list_t **envs);
+extern void ndrx_ndrxconf_envs_envs_free(ndrx_env_list_t **envs);
 
 extern int ndrx_ndrxconf_envs_applyall(ndrx_env_grouplist_t *grouplist, 
         ndrx_env_list_t *envs);
