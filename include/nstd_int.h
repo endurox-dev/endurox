74f4f42f1fa3d9c03678d02880f2537163001052
Madars Vitolins <madars.vitolins@gmail.com>
2018-07-08 21:05:31 +0300

    Updates on config processing

--- include/nstd_int.h
+++ include/nstd_int.h
@@ -1,7 +1,7 @@
 /* 
-** Enduro/X common-config
+** Standard Library internals
 **
-** @file cconfig.h
+** @file nstd_int.h
 ** 
 ** -----------------------------------------------------------------------------
 ** Enduro/X Middleware Platform for Distributed Transaction Processing
@@ -29,65 +29,31 @@
 ** contact@mavimax.com
 ** -----------------------------------------------------------------------------
 */
-#ifndef _CCONFIG_H
-#define	_CCONFIG_H
+
+#ifndef NSTD_INT_H
+#define	NSTD_INT_H
 
 #ifdef	__cplusplus
 extern "C" {
 #endif
 
 /*---------------------------Includes-----------------------------------*/
-#include <sys/stat.h>
-#include <stdint.h>
-#include <exhash.h>
-#include <sys_unix.h>
+#include <ndrstandard.h>
 #include <inicfg.h>
 /*---------------------------Externs------------------------------------*/
 /*---------------------------Macros-------------------------------------*/
-#define NDRX_CCONFIG5 "NDRX_CCONFIG5"
-#define NDRX_CCONFIG4 "NDRX_CCONFIG4"
-#define NDRX_CCONFIG3 "NDRX_CCONFIG3"
-#define NDRX_CCONFIG2 "NDRX_CCONFIG2"
-#define NDRX_CCONFIG1 "NDRX_CCONFIG1"
-#define NDRX_CCONFIG  "NDRX_CCONFIG"
-    
-#define NDRX_CCTAG "NDRX_CCTAG" /* common-config tag */
-    
-#define NDRX_CONF_SECTION_GLOBAL    "@global"
-#define NDRX_CONF_SECTION_DEBUG     "@debug"
-#define NDRX_CONF_SECTION_QUEUE     "@queue"
-#define NDRX_CONF_SECTION_CACHE     "@cache"
-#define NDRX_CONF_SECTION_CACHEDB   "@cachedb"
-#define NDRX_CONF_SECTION_UBFDB     "@ubfdb"
-    
-    
-#define NDRX_CCTAG_MAX      32          /* max len of cctag */
-    
-/*
- * Command for cconfig
- */
-#define NDRX_CCONFIG_CMD_GET        'g' /* get config (default) */
-#define NDRX_CCONFIG_CMD_LIST       'l' /* list config */
-    
 /*---------------------------Enums--------------------------------------*/
 /*---------------------------Typedefs-----------------------------------*/
 /*---------------------------Globals------------------------------------*/
 /*---------------------------Statics------------------------------------*/
 /*---------------------------Prototypes---------------------------------*/
     
-extern NDRX_API int ndrx_cconfig_get(char *section, ndrx_inicfg_section_keyval_t **out);
-extern NDRX_API int ndrx_cconfig_load(void);
-extern NDRX_API ndrx_inicfg_t *ndrx_get_G_cconfig(void);
-extern NDRX_API int ndrx_cconfig_reload(void);
-
-/* for user: */
-extern NDRX_API int ndrx_cconfig_load_general(ndrx_inicfg_t **cfg);
-extern NDRX_API int ndrx_cconfig_get_cf(ndrx_inicfg_t *cfg, char *section, 
-        ndrx_inicfg_section_keyval_t **out);
+extern NDRX_API int ndrx_inicfg_get_subsect_int(ndrx_inicfg_t *cfg, 
+        char **resources, char *section, ndrx_inicfg_section_keyval_t **out);
 
 #ifdef	__cplusplus
 }
 #endif
 
-#endif	/* _CCONFIG_H */
+#endif	/* NSTD_INT_H */
 
