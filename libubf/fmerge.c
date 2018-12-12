e792164b272d7b31187881b0f77f06da80430a4c
Oskars <oskars@popens.lv>
2018-12-12 13:26:14 +0200

    Add Bojoin functionality

--- libubf/fmerge.c
+++ libubf/fmerge.c
@@ -86,7 +86,7 @@ expublic int ndrx_Bupdate (UBFH *p_ub_dst, UBFH *p_ub_src)
         /*
          * Update the occurrence in target buffer.
          */
-        if (EXSUCCEED!=(ret=ndrx_Bchg(p_ub_dst, bfldid, occ, p_fld, len, &chg_state)))
+        if (EXSUCCEED!=(ret=ndrx_Bchg(p_ub_dst, bfldid, occ, p_fld, len, &chg_state, EXFALSE)))
         {
             UBF_LOG(log_debug, "Failed to set %s[%d]", 
                                 ndrx_Bfname_int(bfldid), occ);
@@ -144,4 +144,56 @@ expublic int ndrx_Bconcat (UBFH *p_ub_dst, UBFH *p_ub_src)
     return ret;
 }
 
+/**
+ * Join two buffers, update only existing fields in dest, remove missing fields
+ * @param dest - dest buffer (being modified)
+ * @param src - src buffer (not modified)
+ * @return SUCCEED/FAIL
+ */
+expublic int ndrx_Bjoin (UBFH *dest, UBFH *src)
+{
+    int ret=EXSUCCEED;
+
+    return ret;
+}
+
+/**
+ * Outer join two buffers, update existing, do not remove non-existing fields
+ * @param dest - dest buffer (being modified)
+ * @param src - src buffer (not modified)
+ * @return SUCCEED/FAIL
+ */
+expublic int ndrx_Bojoin (UBFH *dest, UBFH *src)
+{
+    int ret=EXSUCCEED;
+    UBF_header_t *hdr = (UBF_header_t *)dest;
+    char *p_fld;
+    BFLDID bfldid = BFIRSTFLDID;
+    BFLDOCC occ = 0;
+    BFLDLEN len=0;
+    Bnext_state_t state;
+    int nxt_stat;
+    Bfld_loc_info_t chg_state;
+    memset(&chg_state, 0, sizeof(chg_state));
+    memset(&state, 0, sizeof(state));
+    chg_state.last_checked = &hdr->bfldid;
+            
+    while(EXSUCCEED==ret &&
+        1==(nxt_stat=ndrx_Bnext(&state, src, &bfldid, &occ, NULL, &len, &p_fld)))
+    {
+        /*
+         * Update the occurrence in target buffer.
+         */
+        if (EXSUCCEED!=(ret=ndrx_Bchg(dest, bfldid, occ, p_fld, len, &chg_state, EXTRUE)))
+        {
+            UBF_LOG(log_debug, "Failed to set %s[%d]", 
+                                ndrx_Bfname_int(bfldid), occ);
+        }
+    }
+
+    if (EXFAIL==nxt_stat)
+        ret=EXFAIL;
+
+    return ret;
+}
 /* vim: set ts=4 sw=4 et smartindent: */
