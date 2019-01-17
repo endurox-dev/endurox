a051f73d79db8f6520f4c514772c2304ee340133
Einars Leckis <einars.lec@inbox.lv>
2019-01-17 12:59:22 +0200

    Fixes on shms output

--- libnstd/nstd_shmsv.c
+++ libnstd/nstd_shmsv.c
@@ -387,7 +387,8 @@ expublic string_list_t * ndrx_shm_shms_list(key_t ipckey)
         
         if (EXFAIL!=(fd = shmget(key, 0, S_IRWXU | S_IRWXG)))
         {
-            snprintf(segment, sizeof(segment), "%x: %s", key, M_map[i].suffix);
+            snprintf(segment, sizeof(segment), "%x:%d:%s", 
+                    key, fd, M_map[i].suffix);
             if (EXSUCCEED!=ndrx_string_list_add(&ret, segment))
             {
                 NDRX_LOG(log_error, "Failed to add shm segment to list");
