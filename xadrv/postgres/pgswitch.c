041ec47a9a556d13b344b0f1dcfb0e87bf1dea0b
Madars Vitolins <pointer@inbox.lv>
2019-06-16 14:11:53 +0300

    Works on posgresql xa driver

--- xadrv/postgres/pgswitch.c
+++ xadrv/postgres/pgswitch.c
@@ -37,22 +37,69 @@
 #include <ndrstandard.h>
 #include <ndebug.h>
 #include <atmi.h>
-#include <atmi_int.h>
-#include <sys_mqueue.h>
-
-#define __USE_GNU
-#include <dlfcn.h>
 
 #include "atmi_shm.h"
 
 #include <xa.h>
+#include <ecpglib.h>
+#include <pgxa.h>
+#include <thlock.h>
 /*---------------------------Externs------------------------------------*/
 /*---------------------------Macros-------------------------------------*/
+#define CONN_CLOSED 0   /**< The interface is not open  */
+#define CONN_OPEN   1   /**< The interface is open      */
 /*---------------------------Enums--------------------------------------*/
 /*---------------------------Typedefs-----------------------------------*/
 /*---------------------------Globals------------------------------------*/
+expublic __thread char ndrx_G_PG_conname[65]={EXEOS}; /**< connection name    */
 /*---------------------------Statics------------------------------------*/
+
+MUTEX_LOCKDECL(M_conndata_lock);
+exprivate ndrx_pgconnect_t M_conndata; /**< parsed connection data            */
+exprivate int M_conndata_ok = EXFALSE; /**< Is connection parsed ok & cached? */
+
+exprivate __thread int M_status = CONN_CLOSED;  /**< thread based status      */
+
 /*---------------------------Prototypes---------------------------------*/
+
+exprivate int xa_open_entry_stat(char *xa_info, int rmid, long flags);
+exprivate int xa_close_entry_stat(char *xa_info, int rmid, long flags);
+exprivate int xa_start_entry_stat(XID *xid, int rmid, long flags);
+exprivate int xa_end_entry_stat(XID *xid, int rmid, long flags);
+exprivate int xa_rollback_entry_stat(XID *xid, int rmid, long flags);
+exprivate int xa_prepare_entry_stat(XID *xid, int rmid, long flags);
+exprivate int xa_commit_entry_stat(XID *xid, int rmid, long flags);
+exprivate int xa_recover_entry_stat(XID *xid, long count, int rmid, long flags);
+exprivate int xa_forget_entry_stat(XID *xid, int rmid, long flags);
+exprivate int xa_complete_entry_stat(int *handle, int *retval, int rmid, long flags);
+
+exprivate int xa_open_entry(struct xa_switch_t *sw, char *xa_info, int rmid, long flags);
+exprivate int xa_close_entry(struct xa_switch_t *sw, char *xa_info, int rmid, long flags);
+exprivate int xa_start_entry(struct xa_switch_t *sw, XID *xid, int rmid, long flags);
+exprivate int xa_end_entry(struct xa_switch_t *sw, XID *xid, int rmid, long flags);
+exprivate int xa_rollback_entry(struct xa_switch_t *sw, XID *xid, int rmid, long flags);
+exprivate int xa_prepare_entry(struct xa_switch_t *sw, XID *xid, int rmid, long flags);
+exprivate int xa_commit_entry(struct xa_switch_t *sw, XID *xid, int rmid, long flags);
+exprivate int xa_recover_entry(struct xa_switch_t *sw, XID *xid, long count, int rmid, long flags);
+exprivate int xa_forget_entry(struct xa_switch_t *sw, XID *xid, int rmid, long flags);
+exprivate int xa_complete_entry(struct xa_switch_t *sw, int *handle, int *retval, int rmid, long flags);
+
+struct xa_switch_t ndrxpgsw = 
+{ 
+    .name = "ndrxpgsw",
+    .flags = TMNOMIGRATE,
+    .version = 0,
+    .xa_open_entry = xa_open_entry_stat,
+    .xa_close_entry = xa_close_entry_stat,
+    .xa_start_entry = xa_start_entry_stat,
+    .xa_end_entry = xa_end_entry_stat,
+    .xa_rollback_entry = xa_rollback_entry_stat,
+    .xa_prepare_entry = xa_prepare_entry_stat,
+    .xa_commit_entry = xa_commit_entry_stat,
+    .xa_recover_entry = xa_recover_entry_stat,
+    .xa_forget_entry = xa_forget_entry_stat,
+    .xa_complete_entry = xa_complete_entry_stat
+};
 
 /**
  * API entry of loading the driver
@@ -62,7 +109,273 @@
  */
 struct xa_switch_t *ndrx_get_xa_switch(void)
 {
-    return NULL;
+    /* configure other flags */
+    
+    /* prase the config and share it between threads */
+    
+    ndrx_xa_nostartxid(EXTRUE);
+    return &ndrxpgsw;
 }
+
+/**
+ * Open API.
+ * This is called per thread.
+ * The config is written in JSON.
+ * Syntax:
+ * { "url":"unix:postgresql://sql.mydomain.com:5432/mydb", "user":"test", "password":"test1", "compat":"INFORMIX|INFORMIX_SE|PGSQL"}
+ * @param switch 
+ * @param xa_info
+ * @param rmid
+ * @param flags
+ * @return 
+ */
+exprivate int xa_open_entry(struct xa_switch_t *sw, char *xa_info, int rmid, long flags)
+{
+    int ret = XA_OK;
+    static int conn_counter = 0;
+    int connid;
+    if (CONN_OPEN==M_status)
+    {
+        NDRX_LOG(log_error, "Connection is already open");
+        ret=XAER_PROTO;
+        goto out;
+    }
+    
+    /* try parse config */
+    if (!M_conndata_ok)
+    {
+        MUTEX_LOCK_V(M_conndata_lock);
+        
+        if (!M_conndata_ok)
+        {
+            if (EXSUCCEED!=ndrx_pg_xa_cfgparse(xa_info, &M_conndata))
+            {
+                NDRX_LOG(log_error, "Failed to parse Open string!");
+                MUTEX_UNLOCK_V(M_conndata_lock);
+                ret = XAER_INVAL;
+                goto out;
+            }
+            
+            M_conndata_ok = EXTRUE;
+            MUTEX_UNLOCK_V(M_conndata_lock);
+        }
+    }
+    
+    /* generate connection name. The format is following:
+     * YYYYMMDD-HHMISSFFF-%d 
+     */
+    if (EXEOS==ndrx_G_PG_conname[0])
+    {
+        long date;
+        long time;
+        long usec;
+        
+        /* use the same lock for connection naming.. */
+        MUTEX_LOCK_V(M_conndata_lock);
+        connid = conn_counter;
+        
+        conn_counter++;
+        
+        if (conn_counter > 16000)
+        {
+            conn_counter = 0;
+        }
+        
+        MUTEX_UNLOCK_V(M_conndata_lock);
+        
+        ndrx_get_dt_local(&date, &time, &usec);
+        
+        snprintf(ndrx_G_PG_conname, sizeof(conn_counter), "%ld-%ld%ld-%d",
+                date, time, (long)(usec / 1000), connid);
+    }
+    
+    NDRX_LOG(log_debug, "Connection name: [%s]", ndrx_G_PG_conname);
+    
+    /* OK, try to open, with out autocommit please!
+     */
+    if (!ECPGconnect (__LINE__, M_conndata.c, M_conndata.url, M_conndata.user, 
+            M_conndata.password, ndrx_G_PG_conname, EXFALSE))
+    {
+        NDRX_LOG(log_error, "ECPGconnect failed: %s");
+        ret = XAER_RMERR;
+    }
+    
+    M_status = CONN_OPEN;
+    NDRX_LOG(log_info, "Connection [%s] is open", ndrx_G_PG_conname);
+    
+out:
+    return EXFAIL;
+}
+
+/**
+ * Close entry.
+ * @param sw xa switch
+ * @param xa_info close string
+ * @param rmid RM id
+ * @param flags flags
+ * @return xa err
+ */
+exprivate int xa_close_entry(struct xa_switch_t *sw, char *xa_info, int rmid, long flags)
+{
+    
+    
+    return EXFAIL;
+}
+
+
+/**
+ * In this case we need to convert our internal XID format to java XID
+ * and pass it in the call.
+ * @param xa_info
+ * @param rmid
+ * @param flags
+ * @return 
+ */
+exprivate int xa_start_entry(struct xa_switch_t *sw, XID *xid, int rmid, long flags)
+{
+    return EXFAIL;
+}
+
+/**
+ * Terminate XA access
+ * @param sw
+ * @param xid
+ * @param rmid
+ * @param flags
+ * @return 
+ */
+exprivate int xa_end_entry(struct xa_switch_t *sw, XID *xid, int rmid, long flags)
+{
+    return EXFAIL;
+}
+
+/**
+ * Rollback
+ * @param sw
+ * @param xid
+ * @param rmid
+ * @param flags
+ * @return 
+ */
+exprivate int xa_rollback_entry(struct xa_switch_t *sw, XID *xid, int rmid, long flags)
+{
+    return EXFAIL;
+}
+
+/**
+ * prepare
+ * @param sw
+ * @param xid
+ * @param rmid
+ * @param flags
+ * @return 
+ */
+exprivate int xa_prepare_entry(struct xa_switch_t *sw, XID *xid, int rmid, long flags)
+{
+    return EXFAIL;
+}
+
+/**
+ * Commit
+ * @param sw
+ * @param xid
+ * @param rmid
+ * @param flags
+ * @return 
+ */
+exprivate int xa_commit_entry(struct xa_switch_t *sw, XID *xid, int rmid, long flags)
+{
+    return EXFAIL;
+}
+
+/**
+ * Return list of trans.
+ * Java returns full list, but our buffer is limited.
+ * Thus we load the `count' number of items, but we will return the actual
+ * number of java items
+ * @param sw xa switch
+ * @param xid buffer where to load xids
+ * @param count number of xid buffer size
+ * @param rmid resourcemanager id
+ * @param flags flags
+ * @return XA_OK, XERR
+ */
+exprivate int xa_recover_entry(struct xa_switch_t *sw, XID *xid, long count, int rmid, long flags)
+{
+    return EXFAIL;
+}
+
+/**
+ * Forget transaction
+ * @param sw xa switch
+ * @param xid XID
+ * @param rmid RM ID
+ * @param flags flags
+ * @return XA_OK, XERR
+ */
+exprivate int xa_forget_entry(struct xa_switch_t *sw, XID *xid, int rmid, long flags)
+{
+    return EXFAIL;
+}
+
+/**
+ * CURRENTLY NOT USED!!!
+ * @param sw
+ * @param handle
+ * @param retval
+ * @param rmid
+ * @param flags
+ * @return 
+ */
+exprivate int xa_complete_entry(struct xa_switch_t *sw, int *handle, int *retval, int rmid, long flags)
+{
+    return EXFAIL;
+}
+
+/* Static entries */
+exprivate int xa_open_entry_stat( char *xa_info, int rmid, long flags)
+{
+    return xa_open_entry(&ndrxpgsw, xa_info, rmid, flags);
+}
+exprivate int xa_close_entry_stat(char *xa_info, int rmid, long flags)
+{
+    return xa_close_entry(&ndrxpgsw, xa_info, rmid, flags);
+}
+exprivate int xa_start_entry_stat(XID *xid, int rmid, long flags)
+{
+    return xa_start_entry(&ndrxpgsw, xid, rmid, flags);
+}
+
+exprivate int xa_end_entry_stat(XID *xid, int rmid, long flags)
+{
+    return xa_end_entry(&ndrxpgsw, xid, rmid, flags);
+}
+exprivate int xa_rollback_entry_stat(XID *xid, int rmid, long flags)
+{
+    return xa_rollback_entry(&ndrxpgsw, xid, rmid, flags);
+}
+exprivate int xa_prepare_entry_stat(XID *xid, int rmid, long flags)
+{
+    return xa_prepare_entry(&ndrxpgsw, xid, rmid, flags);
+}
+
+exprivate int xa_commit_entry_stat(XID *xid, int rmid, long flags)
+{
+    return xa_commit_entry(&ndrxpgsw, xid, rmid, flags);
+}
+
+exprivate int xa_recover_entry_stat(XID *xid, long count, int rmid, long flags)
+{
+    return xa_recover_entry(&ndrxpgsw, xid, count, rmid, flags);
+}
+exprivate int xa_forget_entry_stat(XID *xid, int rmid, long flags)
+{
+    return xa_forget_entry(&ndrxpgsw, xid, rmid, flags);
+}
+exprivate int xa_complete_entry_stat(int *handle, int *retval, int rmid, long flags)
+{
+    return xa_complete_entry(&ndrxpgsw, handle, retval, rmid, flags);
+}
+
 
 /* vim: set ts=4 sw=4 et smartindent: */
