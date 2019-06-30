c0e44d5c71bb3a13b7e4ee3c4e1bd4e64e4fc1f1
Madars Vitolins <pointer@inbox.lv>
2019-06-30 14:21:46 +0300

    Works on pg testing

--- atmitest/test067_postgres/atmiclt67.c
+++ atmitest/test067_postgres/atmiclt67.c
@@ -48,6 +48,7 @@
 #include <unistd.h>
 #include <nstdutil.h>
 #include "test67.h"
+#include <ecpglib.h>
 /*---------------------------Externs------------------------------------*/
 /*---------------------------Macros-------------------------------------*/
 /*---------------------------Enums--------------------------------------*/
@@ -65,10 +66,12 @@
  */
 long sql_run(char **list, int ret_col_row_1, long *ret_val)
 {
-    PGconn * conn = ECPGget_PGconn("");
+    PGconn * conn = ECPGget_PGconn(NULL);
     long ret = EXSUCCEED;
-    char *command, char *codes;
+    char *command, *codes;
     int i;
+    PGresult *res = NULL;
+    ExecStatusType estat;
     
     /* get connection object */
     if (NULL==conn)
@@ -80,20 +83,61 @@ long sql_run(char **list, int ret_col_row_1, long *ret_val)
     /* process the commands */
     for (i=0; NULL!=list[i]; i+=2)
     {
+        PQclear(res);
+        
         command = list[i];
         codes = list[i+1];
         
         NDRX_LOG(log_debug, "Command [%s] codes [%s]", command, codes);
         
+        res = PQexec(conn, command);
+        
+        estat =PQresultStatus(res);
+        if (PGRES_COMMAND_OK != estat && PGRES_TUPLES_OK != estat) 
+        {
+            char *state = PQresultErrorField(res, PG_DIAG_SQLSTATE);
+            char *ok = "0000";
+            if (NULL==state)
+            {
+                state = ok;
+            }
+            
+            if (0==strstr(codes,state))
+            {
+                NDRX_LOG(log_error, "Statement [%s] failed with [%s] accepted [%s]",
+                        command, state, codes);
+                EXFAIL_OUT(ret);
+            }
+        }
         
     }
     
+    if (ret_col_row_1)
+    {
+        if (PGRES_TUPLES_OK != estat)
+        {
+            NDRX_LOG(log_error, "Requested result, but none provided!");
+            EXFAIL_OUT(ret);
+        }
+        else
+        {
+            *ret_val = atol(PQgetvalue(res, 0, 0));
+            
+            NDRX_LOG(log_info, "Value extracted: %ld", *ret_val);
+        }
+    }
     
 out:
+    PQclear(res);
+
     return ret;
 }
 
-int sql_prepare(void)
+/**
+ * Create tables
+ * @return  EXSUCCEED/EXFAIL
+ */
+int sql_mktab(void)
 {
 
     char *commands[]   = {
@@ -101,27 +145,53 @@ int sql_prepare(void)
             "drop table extest;", "0000;42P01"
             ,"CREATE TABLE extest(userid integer UNIQUE NOT NULL);", "0000"
             ,NULL, NULL};
-    /* TODO: EXEC */
+    return sql_run(commands, EXFALSE, NULL);
 }
 
+/**
+ * Delete from table
+ * @return EXSUCCEED/EXFAIL
+ */
 int sql_delete(void)
 {
     char *commands[]   = {
             /* Command code       Accepted SQL states*/
             "delete from extest;", "0000"
             ,NULL, NULL};
+    
+    return sql_run(commands, EXFALSE, NULL);
 }
 
-
+/**
+ * Count records added to table
+ * @return count/EXFAIL
+ */
 long sql_count(void)
 {
+    long ret_val = EXSUCCEED;
     char *commands[]   = {
             /* Command code       Accepted SQL states*/
             "select count(*) from extest;", "0000"
             ,NULL, NULL};
+    
+    if (EXSUCCEED!=sql_run(commands, EXTRUE, &ret_val))
+    {
+        NDRX_LOG(log_error, "Failed to get count!");
+        ret_val = EXFAIL;
+        goto out;
+    }
+    
+out:
+    return ret_val;
 }
 
-long sql_insert(void)
+/**
+ * Seems like if we try to insert the same unique value in prepared transaction
+ * it will lock up and wait for prep transaction result thus to have some
+ * more data, call sql_insert2
+ * @return 
+ */
+int sql_insert(void)
 {
     /* run some insert */
     
@@ -130,12 +200,37 @@ long sql_insert(void)
             "insert into extest(userid) values ((select COALESCE(max(userid), 1)+1 from extest));", "0000"
             ,NULL, NULL};
     
+    return sql_run(commands, EXFALSE, NULL);
+}
+
+
+/**
+ * In transaction inserts / for suspended
+ * @return 
+ */
+int sql_insert2(void)
+{
+    /* run some insert */
+    
+    char *commands[]   = {
+            /* Command code       Accepted SQL states*/
+            "insert into extest(userid) values (999990);", "0000"
+            ,"insert into extest(userid) values (999991);", "0000"
+            ,"insert into extest(userid) values (999992);", "0000"
+            ,NULL, NULL};
+    
+    return sql_run(commands, EXFALSE, NULL);
 }
 
 /**
- * Do the test call to the server
+ * 1. Do the test call to the server
  * Also we need some test cases from shell processing with stalled commits.
  * Thus needs some parameters to be passed to executable.
+ * 
+ * Additional tests:
+ * 2. postgresql prepare fails / commit shall fail / transaction aborted result
+ * 3. run some inserts commit but leave prepared in db, test the commit/rollback
+ *  command line commands.
  */
 int main(int argc, char** argv)
 {
@@ -143,13 +238,20 @@ int main(int argc, char** argv)
     long rsplen;
     long i;
     int ret=EXSUCCEED;
+    TPTRANID t;
     
+    /* open connection */
     if (EXSUCCEED!=tpopen())
     {
         NDRX_LOG(log_error, "Failed to open: %s", tpstrerror(tperrno));
         EXFAIL_OUT(ret);
     }
     
+    sql_mktab();
+    
+    /**************************************************************************/
+    NDRX_LOG(log_debug, "Test commit");
+    /**************************************************************************/
     if (EXSUCCEED!=tpbegin(60, 0))
     {
         NDRX_LOG(log_error, "Failed to begin: %s", tpstrerror(tperrno));
@@ -171,11 +273,204 @@ int main(int argc, char** argv)
         }
     }
     
+    /* test that we have 0 records here... */
+    if (EXSUCCEED!=(ret=(int)sql_count()))
+    {
+        NDRX_LOG(log_error, "Got count: %d, expected 0", ret);
+        EXFAIL_OUT(ret);
+    }
+        
     if (EXSUCCEED!=tpcommit(0))
     {
         NDRX_LOG(log_error, "Failed to commit: %s", tpstrerror(tperrno));
         EXFAIL_OUT(ret);
     }
+    
+    if (900!=(ret=(int)sql_count()))
+    {
+        NDRX_LOG(log_error, "Got count: %d, expected 900", ret);
+        EXFAIL_OUT(ret);
+    }
+    
+    ret = EXSUCCEED;
+    
+    
+    /**************************************************************************/
+    NDRX_LOG(log_debug, "Test suspend");
+    /**************************************************************************/
+    
+    sql_delete();
+    
+    if (EXSUCCEED!=tpbegin(60, 0))
+    {
+        NDRX_LOG(log_error, "Failed to begin: %s", tpstrerror(tperrno));
+        EXFAIL_OUT(ret);
+    }
+    
+    for (i=0; i<1000; i++)
+    {
+        sql_insert();
+    }
+    
+    if (1000!=(ret=(int)sql_count()))
+    {
+        NDRX_LOG(log_error, "Got count: %d, expected 1000", ret);
+        EXFAIL_OUT(ret);
+    }
+    
+    /* suspend transaction, shall get 0 */
+    
+    if (EXSUCCEED!=tpsuspend(&t, 0L))
+    {
+        NDRX_LOG(log_error, "Failed to suspend: %s", tpstrerror(tperrno));
+        EXFAIL_OUT(ret);
+    }
+    
+    if (0!=(ret=(int)sql_count()))
+    {
+        NDRX_LOG(log_error, "Got count: %d, expected 0", ret);
+        EXFAIL_OUT(ret);
+    }
+    
+    /* start another transaction */
+    NDRX_LOG(log_debug, "running tran within");
+    
+    if (EXSUCCEED!=tpbegin(60, 0))
+    {
+        NDRX_LOG(log_error, "Failed to begin: %s", tpstrerror(tperrno));
+        EXFAIL_OUT(ret);
+    }
+    
+    sql_insert2();
+    
+    if (3!=(ret=(int)sql_count()))
+    {
+        NDRX_LOG(log_error, "Got count: %d, expected 1000", ret);
+        EXFAIL_OUT(ret);
+    }
+    
+    if (EXSUCCEED!=tpcommit(0))
+    {
+        NDRX_LOG(log_error, "Failed to commit: %s", tpstrerror(tperrno));
+        EXFAIL_OUT(ret);
+    }
+    
+    NDRX_LOG(log_debug, "continue with org tran...");
+    if (EXSUCCEED!=tpresume(&t, 0L))
+    {
+        NDRX_LOG(log_error, "Failed to resume: %s", tpstrerror(tperrno));
+        EXFAIL_OUT(ret);
+    }
+    
+    /* as this is new branch tran, we get 0 */
+    if (3!=(ret=(int)sql_count()))
+    {
+        NDRX_LOG(log_error, "Got count: %d, expected 3", ret);
+        EXFAIL_OUT(ret);
+    }
+    
+    if (EXSUCCEED!=tpcommit(0))
+    {
+        NDRX_LOG(log_error, "Failed to commit: %s", tpstrerror(tperrno));
+        EXFAIL_OUT(ret);
+    }
+    
+    /* as this is new branch tran, we get 0 */
+    if (1003!=(ret=(int)sql_count()))
+    {
+        NDRX_LOG(log_error, "Got count: %d, expected 1000", ret);
+        EXFAIL_OUT(ret);
+    }
+    
+    /**************************************************************************/
+    NDRX_LOG(log_debug, "Test abort");
+    /**************************************************************************/
+    
+    sql_delete();
+    
+    if (EXSUCCEED!=tpbegin(60, 0))
+    {
+        NDRX_LOG(log_error, "Failed to begin: %s", tpstrerror(tperrno));
+        EXFAIL_OUT(ret);
+    }
+    
+    for (i=0; i<499; i++)
+    {
+        sql_insert();
+    }
+    
+    /* as this is new branch tran, we get 0 */
+    if (499!=(ret=(int)sql_count()))
+    {
+        NDRX_LOG(log_error, "Got count: %d, expected 499", ret);
+        EXFAIL_OUT(ret);
+    }
+    
+    if (EXSUCCEED!=tpabort(0))
+    {
+        NDRX_LOG(log_error, "Failed to abort: %s", tpstrerror(tperrno));
+        EXFAIL_OUT(ret);
+    }
+    
+    if (0!=(ret=(int)sql_count()))
+    {
+        NDRX_LOG(log_error, "Got count: %d, expected 0", ret);
+        EXFAIL_OUT(ret);
+    }
+    
+    
+    /**************************************************************************/
+    NDRX_LOG(log_debug, "Test timeout");
+    /**************************************************************************/
+    
+    sql_delete();
+    
+    if (EXSUCCEED!=tpbegin(5, 0))
+    {
+        NDRX_LOG(log_error, "Failed to begin: %s", tpstrerror(tperrno));
+        EXFAIL_OUT(ret);
+    }
+    
+    for (i=0; i<10; i++)
+    {
+        sql_insert();
+    }
+    
+    /* as this is new branch tran, we get 0 */
+    if (10!=(ret=(int)sql_count()))
+    {
+        NDRX_LOG(log_error, "Got count: %d, expected 10", ret);
+        EXFAIL_OUT(ret);
+    }
+    
+    /* tmsrv must rollback in this time */
+    sleep(7);
+    
+    if (EXSUCCEED==tpcommit(0))
+    {
+        NDRX_LOG(log_error, "Commit OK, must fail!");
+        EXFAIL_OUT(ret);
+    }
+    
+    if (TPEABORT!=tperrno)
+    {
+        NDRX_LOG(log_error, "Transaction must be aborted!");
+        EXFAIL_OUT(ret);
+    }
+
+    /* as this is new branch tran, we get 0 */
+    if (0!=(ret=(int)sql_count()))
+    {
+        NDRX_LOG(log_error, "Got count: %d, expected 0", ret);
+        EXFAIL_OUT(ret);
+    }
+    
+    /* this shall work out fine, as preared was aborted... */
+    for (i=0; i<10; i++)
+    {
+        sql_insert();
+    }
+    
     
 out:
     
