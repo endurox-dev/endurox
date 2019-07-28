87f7825ee969c943c4130670db819034a62ab53a
Madars Vitolins <pointer@inbox.lv>
2019-07-28 11:06:28 +0300

    Works on cpm shm

--- libatmi/cltshm.c
+++ libatmi/cltshm.c
@@ -67,6 +67,7 @@
 
 exprivate ndrx_shm_t M_clt_shm = {.fd=EXFAIL, .path="", .mem=NULL}; /**< Shared mem block, clt */
 exprivate ndrx_sem_t M_clt_sem = {.semid=0};        /**< Protect the shared memory */
+exprivate int M_attached = EXFALSE;     /**< Are we attached ? */
 
 /*---------------------------Prototypes---------------------------------*/
 
@@ -78,6 +79,12 @@ exprivate ndrx_sem_t M_clt_sem = {.semid=0};        /**< Protect the shared memo
 expublic int ndrx_cltshm_init(int attach_only)
 {
     int ret = EXSUCCEED;
+    
+    if (M_attached)
+    {
+        NDRX_LOG(log_warn, "Already attached to CPM/CLT SHM");
+        goto out;
+    }
     
     M_clt_shm.fd = EXFAIL;
     M_clt_shm.key = G_atmi_env.ipckey + NDRX_SHM_CPM_KEYOFSZ;
@@ -130,7 +137,7 @@ expublic int ndrx_cltshm_init(int attach_only)
         EXFAIL_OUT(ret);
     }
     
-    
+    M_attached = EXTRUE;
  out:
     NDRX_LOG(log_debug, "returns %d", ret);
     return ret;
@@ -205,8 +212,8 @@ expublic int ndrx_cltshm_get_key(char *key, int oflag, int *pos, int *have_value
     if (first)
     {
         conf.elmmax = G_atmi_env.max_clts;
-        conf.elmsz = sizeof(ndrx_svq_map_t);
-        conf.flags_offset = EXOFFSET(ndrx_svq_map_t, flags);
+        conf.elmsz = sizeof(ndrx_clt_shm_t);
+        conf.flags_offset = EXOFFSET(ndrx_clt_shm_t, flags);
         conf.memptr = (void **)&(M_clt_shm.mem);
         conf.p_key_hash=&cltkey_key_hash;
         conf.p_key_debug=&cltkey_key_debug;
@@ -227,6 +234,8 @@ expublic void ndrx_cltshm_detach(void)
     NDRX_LOG(log_debug, "cltshm detach");
     ndrx_shm_close(&M_clt_shm);
     ndrx_sem_close(&M_clt_sem);
+    
+    M_attached = EXFALSE;
 }
 
 /**
@@ -342,6 +351,7 @@ expublic int ndrx_cltshm_setpos(char *key, pid_t pid, short flags, char *procnam
     int pos;
     int have_value;
     int oflag = 0;
+    ndrx_clt_shm_t* el;
     
     if (flags & NDRX_CPM_MAP_ISUSED)
     {
@@ -358,21 +368,25 @@ expublic int ndrx_cltshm_setpos(char *key, pid_t pid, short flags, char *procnam
     {
         
         /* we got a position */
-        
+        el = NDRX_CPM_INDEX(M_clt_shm.mem, pos);
         if (oflag)
         {
-            NDRX_CPM_INDEX(M_clt_shm.mem, pos)->pid = pid;
-            NDRX_STRCPY_SAFE(NDRX_CPM_INDEX(M_clt_shm.mem, pos)->procname, procname);
-            NDRX_CPM_INDEX(M_clt_shm.mem, pos)->flags = flags;
-            time (&(NDRX_CPM_INDEX(M_clt_shm.mem, pos)->stattime));
+            NDRX_STRCPY_SAFE(el->key, key);
+            el->pid = pid;
+            NDRX_STRCPY_SAFE(el->procname, procname);
+            el->flags = flags;
+            time (&(el->stattime));
         }
         else
         {
-            NDRX_CPM_INDEX(M_clt_shm.mem, pos)->flags = flags;
+            el->flags = flags;
         }
         
         ret = EXSUCCEED;
     }
+    
+    NDRX_LOG(log_error, "YOPT LOOKUP!");
+    ndrx_cltshm_get_key(key, oflag, &pos, &have_value);
     
     /* unlock */
     ndrx_sem_rwunlock(&M_clt_sem, 0, NDRX_SEM_TYP_WRITE);
@@ -395,5 +409,102 @@ out:
     
     return ret;
 }
+    
+/**
+ * Scan the shared memory with client data, try to kill
+ * probably the largest part is already dead due to child killings.
+ * After wards, remove the shared segments
+ * @param [in] signals list of signals for kill, terminated with EXFAIL
+ * @param [out] p_was_any if there was any killings set to EXTRUE
+ */
+expublic void ndrx_cltshm_down(int *signals, int *p_was_any)
+{
+    int i, s;
+    ndrx_clt_shm_t *el;
+    string_list_t* cltchildren = NULL;
+    int was_kill = EXFALSE;
+    char *mem_cpy;
+    size_t cpsz;
+    int err;
+    
+    /* but if future api will allow to deregister client, then
+     * shared memory shall be writable to them..
+     */
+    
+    if (ndrx_cltshm_init(EXTRUE))
+    {
+        NDRX_LOG(log_warn, "CLTSHM processing down");
+        
+        cpsz = G_atmi_env.max_clts * sizeof(ndrx_clt_shm_t);
+        mem_cpy = NDRX_MALLOC(cpsz);
+        
+        if (NULL==mem_cpy)
+        {
+            err = errno;
+            NDRX_LOG(log_error, "Failed to malloc %d bytes: %s", 
+                    cpsz, strerror(err));
+            userlog("Failed to malloc %d bytes: %s", 
+                    cpsz, strerror(err));
+            
+            /* nothing todo; */
+            goto out;
+        }
+        
+        if (EXSUCCEED!=ndrx_sem_rwlock(&M_clt_sem, 0, NDRX_SEM_TYP_WRITE))
+        {
+            goto out;
+        }
+        
+        /* thus we need to copy of memory...! */
+        memcpy(mem_cpy, M_clt_shm.mem, cpsz);
+        
+         /* unlock */
+        ndrx_sem_rwunlock(&M_clt_sem, 0, NDRX_SEM_TYP_WRITE);        
+        
+        for (s=0; EXFAIL!=signals[s]; s++)
+        {
+            for (i=0; i<G_atmi_env.max_clts; i++)
+            {
+                el = NDRX_CPM_INDEX(mem_cpy, i);
 
+                if (el->flags & NDRX_CPM_MAP_ISUSED, 
+                        ndrx_sys_is_process_running_by_pid(el->pid))
+                {
+                    /* grab the childs at first loop*/
+                    if (0==s)
+                    {
+                        ndrx_proc_children_get_recursive(&cltchildren, el->pid);
+                    }
+                    
+                    /* kill the process by it self */
+                    kill(el->pid, signals[s]);
+                    was_kill = EXTRUE;
+                } /* if used */
+            } /* for map entry */
+            
+            /* not need to wait after final signal */
+            if (was_kill && EXFAIL!=signals[s+1])
+            {
+                sleep(EX_KILL_SLEEP_SECS);
+            }
+            else
+            {
+                break;
+            }
+        } /* for signal */
+        
+        ndrx_proc_kill_list(cltchildren);
+        ndrx_string_list_free(cltchildren);
+
+        cltchildren = NULL;
+        
+        /* deatch and remove shared mem... */
+        ndrx_cltshm_detach();
+        ndrx_cltshm_remove(EXTRUE);
+    }
+    
+out:
+    *p_was_any = was_kill;
+    return;
+}
 /* vim: set ts=4 sw=4 et smartindent: */
