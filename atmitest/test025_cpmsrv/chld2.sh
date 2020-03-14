e8d02f619889020407004267888162067c507983
Madars Vitolins <pointer@inbox.lv>
2020-03-14 16:00:32 +0200

    Removed sleep forking

--- atmitest/test025_cpmsrv/chld2.sh
+++ atmitest/test025_cpmsrv/chld2.sh
@@ -2,7 +2,14 @@
 
 echo "Starting chld2.sh"
 
-while [[ 1 == 1 ]]; do
-        sleep 1
-done
+#while [[ 1 == 1 ]]; do
+#        sleep 1
+#done
 
+#
+# this will exit once parent is dead - avoid forking
+#
+#
+# Forkless sleep
+#
+read < ./test25_sleep.fifo
--- atmitest/test025_cpmsrv/chld6.sh
+++ atmitest/test025_cpmsrv/chld6.sh
@@ -2,7 +2,12 @@
 
 echo "Starting chld6.sh"
 
-while [[ 1 == 1 ]]; do
-        sleep 1
-done
+#while [[ 1 == 1 ]]; do
+#        sleep 1
+#done
+
+#
+# Forkless sleep
+#
+read < ./test25_sleep.fifo
 
