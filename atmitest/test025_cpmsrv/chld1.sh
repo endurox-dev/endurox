181a7bf18926bf909478ab98a6f8cb6ec7065e48
Madars Vitolins <madars.vitolins@gmail.com>
2020-03-15 11:30:53 +0100

    Added loop for ignore reads

--- atmitest/test025_cpmsrv/chld1.sh
+++ atmitest/test025_cpmsrv/chld1.sh
@@ -9,15 +9,12 @@ trap 'echo Ignoring SIGTERM' SIGTERM
 chld2.sh
 
 
-#while [[ 1 == 1 ]]; do
-#        sleep 1
-#done
-
 #
 # Sleep for ever... - this is due to fact that forks causes at some points
 # to show two processes, thus mes up accounting during run.sh tests
 # we cannot sleep for ever with sleep, because then signals are ignored.
 # thus read from pipe where nothing comes in
 #
-read < ./test.fifo
-
+while [[ 1 == 1 ]]; do
+	read < ./test.fifo
+done
