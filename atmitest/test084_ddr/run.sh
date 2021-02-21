5c6cda348667b40750459a7715e96c54b950730b
Madars Vitolins <pointer@inbox.lv>
2021-02-21 19:45:04 +0200

    Works on ddr testing

---
+++ atmitest/test084_ddr/run.sh
@@ -0,0 +1,194 @@
+#!/bin/bash
+##
+## @brief DDR functionality tests - test launcher
+##
+## @file run.sh
+##
+## -----------------------------------------------------------------------------
+## Enduro/X Middleware Platform for Distributed Transaction Processing
+## Copyright (C) 2009-2016, ATR Baltic, Ltd. All Rights Reserved.
+## Copyright (C) 2017-2019, Mavimax, Ltd. All Rights Reserved.
+## This software is released under one of the following licenses:
+## AGPL (with Java and Go exceptions) or Mavimax's license for commercial use.
+## See LICENSE file for full text.
+## -----------------------------------------------------------------------------
+## AGPL license:
+## 
+## This program is free software; you can redistribute it and/or modify it under
+## the terms of the GNU Affero General Public License, version 3 as published
+## by the Free Software Foundation;
+##
+## This program is distributed in the hope that it will be useful, but WITHOUT ANY
+## WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+## PARTICULAR PURPOSE. See the GNU Affero General Public License, version 3
+## for more details.
+##
+## You should have received a copy of the GNU Affero General Public License along 
+## with this program; if not, write to the Free Software Foundation, Inc., 
+## 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+##
+## -----------------------------------------------------------------------------
+## A commercial use license is available from Mavimax, Ltd
+## contact@mavimax.com
+## -----------------------------------------------------------------------------
+##
+
+export TESTNAME="test084_ddr"
+
+PWD=`pwd`
+if [ `echo $PWD | grep $TESTNAME ` ]; then
+    # Do nothing 
+    echo > /dev/null
+else
+    # started from parent folder
+    pushd .
+    echo "Doing cd"
+    cd $TESTNAME
+fi;
+
+. ../testenv.sh
+
+export TESTDIR="$NDRX_APPHOME/atmitest/$TESTNAME"
+export PATH=$PATH:$TESTDIR
+export NDRX_ULOG=$TESTDIR
+export NDRX_TOUT=10
+export NDRX_SILENT=Y
+
+#
+# Domain 1 - here client will live
+#
+set_dom1() {
+    echo "Setting domain 1"
+    . ../dom1.sh
+    export NDRX_CONFIG=$TESTDIR/ndrxconfig-dom1.xml
+    export NDRX_DMNLOG=$TESTDIR/ndrxd-dom1.log
+    export NDRX_LOG=$TESTDIR/ndrx-dom1.log
+    export NDRX_DEBUG_CONF=$TESTDIR/debug-dom1.conf
+}
+
+
+#
+# Domain 2 - here server will live
+#
+set_dom2() {
+    echo "Setting domain 2"
+    . ../dom2.sh    
+    export NDRX_CONFIG=$TESTDIR/ndrxconfig-dom2.xml
+    export NDRX_DMNLOG=$TESTDIR/ndrxd-dom2.log
+    export NDRX_LOG=$TESTDIR/ndrx-dom2.log
+    export NDRX_DEBUG_CONF=$TESTDIR/debug-dom2.conf
+}
+
+#
+# Generic exit function
+#
+function go_out {
+    echo "Test exiting with: $1"
+    
+    set_dom1;
+    xadmin stop -y
+    xadmin down -y
+
+
+    set_dom2;
+    xadmin stop -y
+    xadmin down -y
+
+    # If some alive stuff left...
+    xadmin killall atmiclt84
+
+    popd 2>/dev/null
+    exit $1
+}
+
+rm *dom*.log
+# Any bridges that are live must be killed!
+xadmin killall tpbridge
+
+set_dom1;
+xadmin down -y
+xadmin start -y || go_out 1
+
+
+set_dom2;
+xadmin down -y
+xadmin start -y || go_out 2
+
+# Have some wait for ndrxd goes in service - wait for connection establishment.
+echo "Wait 10 for conn"
+sleep 10
+RET=0
+
+xadmin psc
+xadmin ppm
+echo "Running off client"
+
+set_dom1;
+
+################################################################################
+# Check routes... (long type)
+################################################################################
+echo "./atmiclt84 -STESTSV -l-200 -gTESTSV@DOM1 -e0 || go_out 1"
+./atmiclt84 -STESTSV -l-200 -gTESTSV@DOM1 -e0 || go_out 1
+
+echo "./atmiclt84 -STESTSV -l1 -gTESTSV@DOM1 -e0 || go_out 2"
+./atmiclt84 -STESTSV -l1 -gTESTSV@DOM1 -e0 || go_out 2
+
+# still in DOM1
+echo "./atmiclt84 -STESTSV -l5 -gTESTSV@DOM1 -e0 || go_out 3"
+./atmiclt84 -STESTSV -l5 -gTESTSV@DOM1 -e0 || go_out 3
+
+# DOM2
+echo "./atmiclt84 -STESTSV -l6 -gTESTSV@DOM2 -e0 || go_out 4"
+./atmiclt84 -STESTSV -l6 -gTESTSV@DOM2 -e0 || go_out 4
+
+# DOM2
+echo "./atmiclt84 -STESTSV -l7 -gTESTSV@DOM2 -e0 || go_out 5"
+./atmiclt84 -STESTSV -l7 -gTESTSV@DOM2 -e0 || go_out 5
+
+# DEFAULT GRP
+echo "./atmiclt84 -STESTSV -l8 -gTESTSV -e0 || go_out 6"
+./atmiclt84 -STESTSV -l8 -gTESTSV -e0 || go_out 6
+
+# DEFAULT GRP
+echo "./atmiclt84 -STESTSV -l99999 -gTESTSV -e0 || go_out 7"
+./atmiclt84 -STESTSV -l99999 -gTESTSV -e0 || go_out 7
+
+# Prev match -> no service
+echo "./atmiclt84 -ST2 -l200 -gT2@DOM3 -e6 || go_out 8"
+./atmiclt84 -ST2 -l200 -gT2@DOM3 -e6 || go_out 8
+
+# DEFAULT MATCH
+echo "./atmiclt84 -ST2 -l201 -gT2 -e0 || go_out 9"
+./atmiclt84 -ST2 -l201 -gT2 -e0 || go_out 9
+
+# TODO: check error 10 (cannot route)
+
+################################################################################
+# Check routes... (double type)
+################################################################################
+
+################################################################################
+# Check routes... (string type)
+################################################################################
+
+################################################################################
+# Advertise checks.
+################################################################################
+
+
+#
+# TODO: Needs tests for unadvertise ... OK
+#
+
+# Catch is there is test error!!!
+if [ "X`grep TESTERROR *.log`" != "X" ]; then
+    echo "Test error detected!"
+    RET=-2
+fi
+
+go_out $RET
+
+
+# vim: set ts=4 sw=4 et smartindent:
+
