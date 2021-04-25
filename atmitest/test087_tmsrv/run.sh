69e2301cb27672184f3406cf9f07d0c2b8a172fb
Madars Vitolins <pointer@inbox.lv>
2021-04-26 01:55:30 +0300

    Fixes on unit testing

--- atmitest/test087_tmsrv/run.sh
+++ atmitest/test087_tmsrv/run.sh
@@ -219,7 +219,7 @@ fi
 
 export NDRX_HOME=.
 export PATH=$PATH:$PWD/../../buildtools
-export CFLAGS="-I../../include -L../../libatmi -L../../libubf -L../../tmsrv -L../../libatmisrv -L../../libexuuid -L../../libexthpool -L../../libnstd $COMPFLAGS"
+export CFLAGS="-I../../include -L../../libatmi -L../../libatmiclt -L../../libubf -L../../tmsrv -L../../libatmisrv -L../../libexuuid -L../../libexthpool -L../../libnstd $COMPFLAGS"
 
 clean_logs;
 rm *.log
