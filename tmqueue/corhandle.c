88185e5d00694d4ed8c991d0df59ca14ac6343ff
Madars Vitolins <madars.vitolins@gmail.com>
2021-07-22 20:45:33 +0300

    Works on correlation handling

---
+++ tmqueue/corhandle.c
@@ -0,0 +1,60 @@
+/**
+ * @brief Correlation id queue handle
+ *
+ * @file corhandle.c
+ */
+/* -----------------------------------------------------------------------------
+ * Enduro/X Middleware Platform for Distributed Transaction Processing
+ * Copyright (C) 2009-2016, ATR Baltic, Ltd. All Rights Reserved.
+ * Copyright (C) 2017-2019, Mavimax, Ltd. All Rights Reserved.
+ * This software is released under one of the following licenses:
+ * AGPL (with Java and Go exceptions) or Mavimax's license for commercial use.
+ * See LICENSE file for full text.
+ * -----------------------------------------------------------------------------
+ * AGPL license:
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License, version 3 as published
+ * by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU Affero General Public License, version 3
+ * for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along 
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * -----------------------------------------------------------------------------
+ * A commercial use license is available from Mavimax, Ltd
+ * contact@mavimax.com
+ * -----------------------------------------------------------------------------
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <regex.h>
+#include <utlist.h>
+#include <stdarg.h>
+
+#include <ndebug.h>
+#include "qcommon.h"
+#include "tmqd.h"
+/*---------------------------Externs------------------------------------*/
+/*---------------------------Macros-------------------------------------*/
+/*---------------------------Enums--------------------------------------*/
+/*---------------------------Typedefs-----------------------------------*/
+/*---------------------------Globals------------------------------------*/
+/*---------------------------Statics------------------------------------*/
+/*---------------------------Prototypes---------------------------------*/
+
+/* TODO: 
+- add to queue corhash/queue
+- remove from queue corash/queue (incl free-up of hash)
+- dequeue from queue corhash  LIFO/FIFO
+*/
+
+
+/* vim: set ts=4 sw=4 et smartindent: */
