b052d3e9628b13cab487889056203eabbae4a9d3
Lauris Vitolins <contact@mavimax.com>
2022-01-02 00:48:09 +0200

    Support #753 keep default signal handlers for forked processes

--- ndrxd/pmodel.c
+++ ndrxd/pmodel.c
@@ -908,6 +908,9 @@ expublic int start_process(command_startstop_t *cmd_call, pm_node_t *p_pm,
         
 	/* reset signal handler so that for new processes there is scratch start */
 	signal(SIGCHLD, SIG_DFL);
+	signal(SIGINT, SIG_DFL);
+	signal(SIGTERM, SIG_DFL);
+	signal(SIGHUP, SIG_DFL);
 
         /* Bug #176: close parent resources - not needed any more... */
         ndrxd_shm_close_all();
