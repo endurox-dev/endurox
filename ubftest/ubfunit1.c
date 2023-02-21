68ada91688fe3f65f087cfce6ef511504cb09621
Madars Vitolins <madars@mavimax.com>
2023-02-22 01:52:38 +0200

    Copyright update for year 2023

--- ubftest/ubfunit1.c
+++ ubftest/ubfunit1.c
@@ -5,7 +5,7 @@
 /* -----------------------------------------------------------------------------
  * Enduro/X Middleware Platform for Distributed Transaction Processing
  * Copyright (C) 2009-2016, ATR Baltic, Ltd. All Rights Reserved.
- * Copyright (C) 2017-2019, Mavimax, Ltd. All Rights Reserved.
+ * Copyright (C) 2017-2023, Mavimax, Ltd. All Rights Reserved.
  * This software is released under one of the following licenses:
  * AGPL (with Java and Go exceptions) or Mavimax's license for commercial use.
  * See LICENSE file for full text.
