63e779b5b3e79a0faf6da6e8fc2d7267c2b1d99e
Madars Vitolins <madars@mavimax.com>
2023-02-26 10:29:31 +0200

    Date update

--- include/atmi_int.h
+++ include/atmi_int.h
@@ -239,7 +239,7 @@ extern "C" {
                         __DATE__, __TIME__, ndrx_epoll_mode(), NDRX_BUILD_OS_NAME, sizeof(void *)*8);\
         fprintf(stderr, "Enduro/X Middleware Platform for Distributed Transaction Processing\n");\
         fprintf(stderr, "Copyright (C) 2009-2016 ATR Baltic Ltd.\n");\
-        fprintf(stderr, "Copyright (C) 2017-2022 Mavimax Ltd. All Rights Reserved.\n\n");\
+        fprintf(stderr, "Copyright (C) 2017-2023 Mavimax Ltd. All Rights Reserved.\n\n");\
         fprintf(stderr, "This software is released under one of the following licenses:\n");\
         fprintf(stderr, "AGPLv3 (exceptions for Java, Go) or Mavimax license for commercial use.\n\n");\
     }
