91621dd362d3b4496b7cc2990095292ab55b319f
Madars Vitolins <mvitolin@37l4247f27-25.elko.local>
2023-08-06 01:15:26 +0300

    Works on singleton group support

--- libnstd/singlegrp.c
+++ libnstd/singlegrp.c
@@ -41,6 +41,13 @@
 #include <sys/time.h>
 /*---------------------------Externs------------------------------------*/
 /*---------------------------Macros-------------------------------------*/
+
+/* have macro to get the pointer to the group in shared memory */
+#define NDRX_SG_GET_PTR(sg_no) \
+    (ndrx_sg_shm_t *)((char *)ndrx_G_shmcfg->commands \
+            + sizeof(ndrx_G_shmcfg->commands[0])*ndrx_G_libnstd_cfg.lcfmax \
+            + sizeof(ndrx_sg_shm_t)*(sg_no-1))
+
 /*---------------------------Enums--------------------------------------*/
 /*---------------------------Typedefs-----------------------------------*/
 /*---------------------------Globals------------------------------------*/
@@ -109,6 +116,21 @@ expublic void ndrx_sg_reset(void)
 }
 
 /**
+ * If group was locked, but for some reason
+ * we face a condition that we group shall be unlocked.
+ * This resets key statistics of the group entry.
+ * @param sg resolved group ptr
+ * @param reason reason code, why group was unlocked
+ */
+expublic void ndrx_sg_unlock(ndrx_sg_shm_t * sg, int reason)
+{
+    atomic_store(&sg->is_srv_booted, EXFALSE);
+    atomic_store(&sg->is_clt_booted, EXFALSE);
+    atomic_store(&sg->is_locked, EXFALSE);
+    atomic_store(&sg->reason, reason);
+}
+
+/**
  * Load all fields from shared memory to local copy
  * in atomic way.
  * @param sg local copy
@@ -128,38 +150,34 @@ exprivate void ndrx_sg_load(ndrx_sg_shm_t * sg, ndrx_sg_shm_t * sg_shm)
 }
 
 /**
- * Is given group locked?
- * Get the ptr to ndrx_sg_shm_t and check following items:
- * - field is_locked is EXTRUE
- * - last_refresh (substracted current boot time / gettimeofday()) is less ndrx_G_libnstd_cfg.sgrefresh
- * - if maintenance mode is enabled, then we do not perform any further checks, other than lock status.
+ * Check is group locked (internal version)
  * @param singlegrp_no single group number
- * @param reference_file this is refrence file name, used to check arn't there any future modifications
- *  applied, or file missing?
- * @param flags biwtise flags, NDRX_SG_CHK_PID used for additional check of exsinglesv running.
- * @return EXFAIL/EXFALSE (not locked)/EXTRUE (locked)
+ * @param sg optional resolved group from shared memory (will be used if not NULL, instead of the lookup by no)
+ * @param reference_file file for which to check future modification date
+ * @param flags NDRX_SG_CHK_PID check for pid, default 0
  */
-expublic int ndrx_sg_is_locked(int singlegrp_no, char *reference_file, long flags)
+exprivate int ndrx_sg_is_locked_int(int singlegrp_no, ndrx_sg_shm_t * sg, char *reference_file, long flags)
 {
     int ret = EXFALSE;
-    ndrx_sg_shm_t * sg, sg_local;
+    ndrx_sg_shm_t sg_local;
     struct timespec ts;
     
-    ndrx_realtime_get(&ts);
-
-    if (0==singlegrp_no)
-    {
-        /* group 0 is always locked */
-        ret=EXTRUE;
-        goto out;
-    }
-
-    sg=ndrx_sg_get(singlegrp_no);
-
     if (NULL==sg)
     {
-        ret=EXFAIL;
-        goto out;
+        if (0==singlegrp_no)
+        {
+            /* group 0 is always locked */
+            ret=EXTRUE;
+            goto out;
+        }
+
+        sg=ndrx_sg_get(singlegrp_no);
+
+        if (NULL==sg)
+        {
+            ret=EXFAIL;
+            goto out;
+        }
     }
 
     if (!sg->is_locked)
@@ -171,18 +189,19 @@ expublic int ndrx_sg_is_locked(int singlegrp_no, char *reference_file, long flag
 
     /* load all fields from shared memory to local copy */
     ndrx_sg_load(&sg_local, sg);
+    ndrx_realtime_get(&ts);
 
     /* validate the lock */
     if (llabs( (long long)ts.tv_sec-(long long)sg_local.last_refresh) > ndrx_G_libnstd_cfg.sgrefreshmax)
     {
         /* Mark system as not locked anymor! */
-        atomic_store(&sg->is_locked, EXFALSE);
+        ndrx_sg_unlock(sg, NDRX_SG_RSN_EXPIRED);
 
-        NDRX_LOG(log_error, "ERROR: Lock for singleton group %d is expired "
+        NDRX_LOG(log_error, "ERROR: Lock for singleton group %d is inconsistent "
                 "(did not refresh in %d sec)! "
                 "Marking group as not locked!", singlegrp_no, ndrx_G_libnstd_cfg.sgrefreshmax);
 
-        userlog("ERROR: Lock for singleton group %d is expired "
+        userlog("ERROR: Lock for singleton group %d is inconsistent "
                 "(did not refresh in %d sec)! "
                 "Marking group as not locked!", singlegrp_no, ndrx_G_libnstd_cfg.sgrefreshmax);
 
@@ -196,13 +215,13 @@ expublic int ndrx_sg_is_locked(int singlegrp_no, char *reference_file, long flag
         if (!ndrx_sys_is_process_running_by_pid(sg_local.lockprov_pid))
         {
             /* Mark system as not locked anymor! */
-            atomic_store(&sg->is_locked, EXFALSE);
+            ndrx_sg_unlock(sg, NDRX_SG_RSN_NOPID);
 
-            NDRX_LOG(log_error, "ERROR: Lock for singleton group %d is expired "
+            NDRX_LOG(log_error, "ERROR: Lock for singleton group %d is inconsistent "
                     "(lock provider PID %d is not running)! "
                     "Marking group as not locked!", singlegrp_no, sg_local.lockprov_pid);
 
-            userlog("ERROR: Lock for singleton group %d is expired "
+            userlog("ERROR: Lock for singleton group %d is inconsistent "
                     "(lock provider PID %d is not running)! "
                     "Marking group as not locked!", singlegrp_no, sg_local.lockprov_pid);
 
@@ -225,14 +244,14 @@ expublic int ndrx_sg_is_locked(int singlegrp_no, char *reference_file, long flag
         if (0!=rc)
         {
             int err=errno;
-            /* Mark system as not locked anymor! */
-            atomic_store(&sg->is_locked, EXFALSE);
+            /* Mark system as not locked anymore! */
+            ndrx_sg_unlock(sg, NDRX_SG_RSN_REFNOFILE);
 
-            NDRX_LOG(log_error, "ERROR: Lock for singleton group %d is expired "
+            NDRX_LOG(log_error, "ERROR: Lock for singleton group %d is inconsistent "
                     "(reference file [%s] failed to open: [%s])! "
                     "Marking group as not locked!", singlegrp_no, strerror(err), reference_file);
 
-            userlog("ERROR: Lock for singleton group %d is expired "
+            userlog("ERROR: Lock for singleton group %d is inconsistent "
                     "(reference file [%s] is missing: [%s])! "
                     "Marking group as not locked!", singlegrp_no, strerror(err), reference_file);
 
@@ -249,13 +268,13 @@ expublic int ndrx_sg_is_locked(int singlegrp_no, char *reference_file, long flag
         if (st.st_mtime > now.tv_sec)
         {
             /* Mark system as not locked anymor! */
-            atomic_store(&sg->is_locked, EXFALSE);
+            ndrx_sg_unlock(sg, NDRX_SG_RSN_REFFFUT);
 
-            NDRX_LOG(log_error, "ERROR: Lock for singleton group %d is expired "
+            NDRX_LOG(log_error, "ERROR: Lock for singleton group %d is inconsistent "
                     "(reference file %s is in future)! "
                     "Marking group as not locked!", singlegrp_no, reference_file);
 
-            userlog("ERROR: Lock for singleton group %d is expired "
+            userlog("ERROR: Lock for singleton group %d is inconsistent "
                     "(reference file %s is in future)! "
                     "Marking group as not locked!", singlegrp_no, reference_file);
 
@@ -272,6 +291,25 @@ out:
 }
 
 /**
+ * Is given group locked?
+ * Get the ptr to ndrx_sg_shm_t and check following items:
+ * - field is_locked is EXTRUE
+ * - last_refresh (substracted current boot time / gettimeofday()) is less ndrx_G_libnstd_cfg.sgrefresh
+ * - if maintenance mode is enabled, then we do not perform any further checks, other than lock status.
+ * @param singlegrp_no single group number
+ * @param reference_file this is refrence file name, used to check arn't there any future modifications
+ *  applied, or file missing?
+ * @param flags biwtise flags, NDRX_SG_CHK_PID used for additional check of exsinglesv running.
+ * @return EXFAIL/EXFALSE (not locked)/EXTRUE (locked)
+ */
+
+expublic int ndrx_sg_is_locked(int singlegrp_no, char *reference_file, long flags)
+{
+    return ndrx_sg_is_locked_int(singlegrp_no, NULL, reference_file, flags);
+}
+
+
+/**
  * Number of singleton groups.
  * We count in group 0, which is virtual group and it is always locked
  */
@@ -283,10 +321,16 @@ expublic int ndrx_sg_nrgroups()
 /**
  * Take a snapshoot of all groups, to be used by
  * batch startup processes (so that batch is started with the single status)
+ * @param lock_status_out where to put the group statuses
+ * @param lock_status_out_len length of the lock_status_out
+ * @param flags 0 or (NDRX_SG_NOORDER_LCK -> if ordering not required, assume locked)
+ *  additonally may pass NDRX_SG_CHK_PID to check for lock provider pid
  */
-expublic void ndrx_sg_get_lock_snapshoot(int *lock_status_out, int *lock_status_out_len)
+expublic void ndrx_sg_get_lock_snapshoot(int *lock_status_out, int *lock_status_out_len, long flags)
 {
     int i=0;
+    int locked;
+    ndrx_sg_shm_t *p;
 
     /* fix up the output len */
     if (*lock_status_out_len>ndrx_G_libnstd_cfg.sgmax)
@@ -296,11 +340,49 @@ expublic void ndrx_sg_get_lock_snapshoot(int *lock_status_out, int *lock_status_
 
     for (i=0; i<*lock_status_out_len; i++)
     {
-        lock_status_out[i] = ndrx_sg_is_locked(i+1, NULL, 0);
+        p=ndrx_sg_get(i);
+
+        if ( (p->flags & NDRX_SG_NO_ORDER) && (flags & NDRX_SG_NOORDER_LCK))
+        {
+            lock_status_out[i]=EXTRUE;
+        }
+        else
+        {
+            lock_status_out[i]=ndrx_sg_is_locked_int(i, p, NULL, (flags & NDRX_SG_CHK_PID));
+        }
     }
 
     return;
 }
 
+/**
+ * Set the bootflag on.
+ * This assumes that shared memory is already attached.
+ * If the LCF was not attached, then ndrxd would not get so far.
+ * @param singlegrp_no single group number
+ */
+expublic void ndrx_sg_bootflag_clt_set(int singlegrp_no)
+{
+    if (singlegrp_no>0)
+    {
+        ndrx_sg_shm_t * sg = NDRX_SG_GET_PTR(singlegrp_no);
+        atomic_store(&sg->is_clt_booted, EXTRUE);
+    }
+}
+
+/**
+ * Set the bootflag on.
+ * This assumes that shared memory is already attached.
+ * If the LCF was not attached, then ndrxd would not get so far.
+ * @param singlegrp_no single group number
+ */
+expublic void ndrx_sg_bootflag_srv_set(int singlegrp_no)
+{
+    if (singlegrp_no>0)
+    {
+        ndrx_sg_shm_t * sg = NDRX_SG_GET_PTR(singlegrp_no);
+        atomic_store(&sg->is_srv_booted, EXTRUE);
+    }
+}
 
 /* vim: set ts=4 sw=4 et smartindent: */
