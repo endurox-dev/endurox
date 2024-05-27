f1a279027be8d2a3a1e728a2b0925319cd9f8dc2
“Lauris <lauris@mavimax.com>
2024-05-27 09:37:01 +0300

    Added missing file

---
+++ atmitest/test031_logging/atmiclt31_masking.c
@@ -0,0 +1,72 @@
+/**
+ * @brief Verify the log masking routines
+ *
+ * @file atmiclt31_masking.c
+ */
+/* -----------------------------------------------------------------------------
+ * Enduro/X Middleware Platform for Distributed Transaction Processing
+ * Copyright (C) 2009-2016, ATR Baltic, Ltd. All Rights Reserved.
+ * Copyright (C) 2017-2023, Mavimax, Ltd. All Rights Reserved.
+ * This software is released under one of the following licenses:
+ * AGPL (with Java and Go exceptions) or Mavimax's license for commercial use.
+ * See LICENSE file for full text.
+ * -----------------------------------------------------------------------------
+ * AGPL license:
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License, version 3 as published
+ * by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU Affero General Public License, version 3
+ * for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along 
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * -----------------------------------------------------------------------------
+ * A commercial use license is available from Mavimax, Ltd
+ * contact@mavimax.com
+ * -----------------------------------------------------------------------------
+ */
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <memory.h>
+#include <math.h>
+#include <unistd.h>
+
+#include <atmi.h>
+#include <ubf.h>
+#include <ndebug.h>
+#include <ndebugcmn.h>
+#include <test.fd.h>
+#include <ndrstandard.h>
+#include <nstdutil.h>
+#include <exassert.h>
+#include <nstd_int.h>
+/*---------------------------Externs------------------------------------*/
+/*---------------------------Macros-------------------------------------*/
+/*---------------------------Enums--------------------------------------*/
+/*---------------------------Typedefs-----------------------------------*/
+/*---------------------------Globals------------------------------------*/
+/*---------------------------Statics------------------------------------*/
+/*---------------------------Prototypes---------------------------------*/
+
+/*
+ * Do the test call to the server
+ */
+int main(int argc, char** argv)
+{
+    /* print current log level */
+    TP_LOG(log_info, "Masking output1: [%s]", ndrx_dbg_mask(0, "hello masking", NULL, 0));
+    TP_LOG(log_info, "Masking output2: [%s]", ndrx_dbg_mask(0, NULL, NULL, 0));
+    TP_LOG(log_info, "Masking output3: [%s]", ndrx_dbg_mask(NDRX_DBG_MASK_SLOT_MAX, "invalid slot 1", NULL, 0));
+    TP_LOG(log_info, "Masking output4: [%s]", ndrx_dbg_mask(-1, "invalid slot 2", NULL, 0));
+    
+    return EXSUCCEED;
+}
+
+/* vim: set ts=4 sw=4 et smartindent: */
